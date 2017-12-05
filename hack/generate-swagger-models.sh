8abeff8018b2004d07874f1f7a076e8d350e3910
YaoZengzeng <yaozengzeng@zju.edu.cn>
2017-12-05 16:39:26 +0800

    remove the commit number in generate-swagger-models.sh
    Signed-off-by: YaoZengzeng <yaozengzeng@zju.edu.cn>

--- hack/generate-swagger-models.sh
+++ hack/generate-swagger-models.sh
@@ -3,7 +3,6 @@
 # Here is details of the swagger binary we use.
 # $ swagger version
 # version: 0.12.0
-# commit: 8135eb6728e43b73489e80f94426e6d387809502
 
 # Get the absolute path of this file
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
