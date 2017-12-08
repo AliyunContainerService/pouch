166fd1d733f4bae603ea0f9ced9e81c106d2f366
Wei Fu <fhfuwei@163.com>
2017-12-08 13:41:12 +0800

    refactor: use oci v1 reference to parse image name
    Signed-off-by: Wei Fu <fhfuwei@163.com>

---
+++ pkg/reference/reference.go
@@ -0,0 +1,38 @@
+package reference
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	// ErrInvalid is used to return error if reference is invalid
+	ErrInvalid = errors.New("invalid reference")
+
+	// defaultTag is latest if there is no tag
+	defaultTag = "latest"
+)
+
+// Ref represents image name.
+type Ref struct {
+	Name string
+	Tag  string
+}
+
+// String returns reference in string.
+func (r Ref) String() string {
+	return fmt.Sprintf("%s:%s", r.Name, r.Tag)
+}
+
+// Parse a string into reference.
+func Parse(s string) (Ref, error) {
+	if ok := regRef.MatchString(s); !ok {
+		return Ref{}, ErrInvalid
+	}
+
+	tag := defaultTag
+	if loc := regTag.FindStringIndex(s); loc != nil {
+		s, tag = s[:loc[0]], s[loc[0]+1:]
+	}
+	return Ref{Name: s, Tag: tag}, nil
+}
---
+++ pkg/reference/reference_test.go
@@ -0,0 +1,71 @@
+package reference
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParse(t *testing.T) {
+	type tCase struct {
+		name     string
+		input    string
+		expected Ref
+		err      error
+	}
+
+	for _, tc := range []tCase{
+		{
+			name:  "Normal",
+			input: "docker.io/library/nginx:alpine",
+			expected: Ref{
+				Name: "docker.io/library/nginx",
+				Tag:  "alpine",
+			},
+			err: nil,
+		}, {
+			name:  "Localhost registry",
+			input: "localhost:80/nginx:alpine",
+			expected: Ref{
+				Name: "localhost:80/nginx",
+				Tag:  "alpine",
+			},
+			err: nil,
+		}, {
+			name:  " : in path",
+			input: "localhost:80/nginx:nginx/alpine",
+			expected: Ref{
+				Name: "localhost:80/nginx:nginx/alpine",
+				Tag:  "latest",
+			},
+			err: nil,
+		}, {
+			name:     "Contains scheme",
+			input:    "http://docker.io/library/nginx:alpine",
+			expected: Ref{},
+			err:      ErrInvalid,
+		}, {
+			name:     "Contains query",
+			input:    "docker.io/library/nginx?tag=alpine",
+			expected: Ref{},
+			err:      ErrInvalid,
+		}, {
+			name:     "Contains fragment",
+			input:    "docker.io/library/nginx#tag=alpine",
+			expected: Ref{},
+			err:      ErrInvalid,
+		}, {
+			name:  "Punycode",
+			input: "xn--bcher-kva.tld/redis:3",
+			expected: Ref{
+				Name: "xn--bcher-kva.tld/redis",
+				Tag:  "3",
+			},
+			err: nil,
+		},
+	} {
+		ref, err := Parse(tc.input)
+		assert.Equal(t, tc.err, err, tc.name)
+		assert.Equal(t, tc.expected, ref, tc.name)
+	}
+}
