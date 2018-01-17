466a12f68778ee737dbc863f010549f224196680
Zhanchun Zhang <zzchun@zju.edu.cn>
2018-01-17 16:29:41 +0800

    feature: build rpm package for pouch
    Signed-off-by: Zhanchun Zhang <zzchun@zju.edu.cn>

---
+++ hack/package/rpm/scripts/after-install.sh
@@ -0,0 +1,11 @@
+if [ $1 -eq 1 ] ; then
+	systemctl preset pouch > /dev/null 2>&1
+	
+fi
+if ! getent group pouch > /dev/null; then
+	groupadd --system pouch
+fi
+
+if [ ! -d "/var/lib/lxcfs" ] ; then
+    mkdir -p /var/lib/lxcfs
+fi
