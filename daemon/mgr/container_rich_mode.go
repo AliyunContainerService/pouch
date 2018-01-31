7367859d686c69fad6dc570aa4f2c4ef6f9d8e0b
allen.wang <allen.wq@alipay.com>
2018-01-31 19:26:09 +0800

    feature: add rich container mode in pouchd, and translate it to runc by spec

---
+++ daemon/mgr/container_rich_mode.go
@@ -0,0 +1,51 @@
+package mgr
+
+import (
+	"fmt"
+	"github.com/alibaba/pouch/apis/types"
+)
+
+const(
+	rich_mode_env = "rich_mode=true"
+	rich_mode_launch_env = "rich_mode_launch_manner"
+
+	//for ali internal
+	inter_rich_mode_env = "ali_run_mode=common_vm"
+)
+
+func richContainerMode(c *ContainerMeta) []string {
+	var(
+		ret []string = []string{}
+		setRichMode = false
+		richMode = ""
+	)
+
+	envs := c.Config.Env
+
+	//if set inter_rich_mode_env, you can also run in rich container mode
+	for _,e := range envs {
+		if e == inter_rich_mode_env {
+			setRichMode = true
+			break
+		}
+	}
+
+	if c.Config.Rich {
+		setRichMode = true
+	}
+
+	if c.Config.RichMode != "" {
+		richMode = c.Config.RichMode
+	}
+
+	//if not set rich mode manner, default set dumb-init
+	if richMode == "" {
+		richMode = types.ContainerConfigRichModeDumbInit
+	}
+
+	if setRichMode {
+		ret = append(ret, rich_mode_env, fmt.Sprintf("%s=%s", rich_mode_launch_env, richMode))
+	}
+
+	return ret
+}
