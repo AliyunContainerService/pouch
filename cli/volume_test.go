74120445a9ef183162167717f9e875761f2e5062
letty <letty.ll@alibaba-inc.com>
2018-02-13 16:25:24 +0800

    test: add unit test for network
    Signed-off-by: letty <letty.ll@alibaba-inc.com>

--- cli/volume_test.go
+++ cli/volume_test.go
@@ -16,7 +16,62 @@ func Test_parseVolume(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-	// TODO: Add test cases.
+		{
+			name: "parseVolumeTestOk",
+			args: args{
+				volumeCreateConfig: &types.VolumeCreateConfig{
+					DriverOpts: map[string]string{},
+					Labels:     map[string]string{},
+				},
+				v: &VolumeCreateCommand{
+					labels:    []string{"a=b"},
+					options:   []string{"1=2"},
+					selectors: []string{"c=d"},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "parseVolumeTestInvalid1",
+			args: args{
+				volumeCreateConfig: &types.VolumeCreateConfig{
+					DriverOpts: map[string]string{},
+					Labels:     map[string]string{},
+				},
+				v: &VolumeCreateCommand{
+					labels: []string{"ab"},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "parseVolumeTestInvalid2",
+			args: args{
+				volumeCreateConfig: &types.VolumeCreateConfig{
+					DriverOpts: map[string]string{},
+					Labels:     map[string]string{},
+				},
+				v: &VolumeCreateCommand{
+					options: []string{"1&2"},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "parseVolumeTestInvalid3",
+			args: args{
+				volumeCreateConfig: &types.VolumeCreateConfig{
+					DriverOpts: map[string]string{},
+					Labels:     map[string]string{},
+				},
+				v: &VolumeCreateCommand{
+					labels:    []string{"ab"},
+					options:   []string{"1&2"},
+					selectors: []string{"c==d"},
+				},
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
