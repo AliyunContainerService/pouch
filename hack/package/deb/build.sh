7f7923d5882f479d45e9a8a139e914f1cae4e023
Zhanchun Zhang <zzchun@zju.edu.cn>
2018-03-05 15:42:31 +0800

    fix: optimize the way of packaging
    Signed-off-by: Zhanchun Zhang <zzchun@zju.edu.cn>

--- hack/package/deb/build.sh
+++ hack/package/deb/build.sh
@@ -1,39 +1,20 @@
 #!/usr/bin/env sh
 
 set -e
-# This script is to build pouch rpm package as follows,
-# Following the below command to build rpm
-# 1. Build pouch:deb image
-#	cd pouch
-#	docker build -t pouch:deb -f hack/package/deb/ubuntu-xenial/Dockerfile.x86_64 .
-# 2. Mount a directory which contains gpg keys, eg
-#    $ tree /root/deb/
-#		deb
-#		├── keys
-#		│   ├── gpg
-#		│   └── secretkey
-# 3. run pouch:deb to generate deb package
-#	 docker run -it -e VERSION="0.1.0" -v /root/deb/:/root/deb pouch:deb bash -c hack/package/deb/build.sh
-# 4. In this example, rpm package will be output in '/root/deb/package/' directory
-
-DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 TMP=$(mktemp -d /tmp/pouch.XXXXXX)
-
+DEFAULT_GPG_KEY=${GPGKEY:-"439AE9EC"}
 MOUNTDIR=/root/deb
-PACKAGEDIR=/root/deb/package
+PACKAGEDIR=/root/deb/package/deb/
 BASEDIR=/go/src/github.com/alibaba/pouch
 
-rm -rf $BASEDIR/debian && cp -r $DIR/debian $BASEDIR/
-rm -rf $BASEDIR/systemd && cp -r $DIR/systemd $BASEDIR/
-
-if [ ! -e $$BASEDIR/Makefile.bak ] ;then
-	mv $BASEDIR/Makefile $BASEDIR/Makefile.bak
-	cp $DIR/Makefile $BASEDIR/Makefile
-fi
+# prepare files that used to build deb packages
+rm -rf $BASEDIR/debian && cp -r $BASEDIR/hack/package/deb/debian $BASEDIR/
+rm -rf $BASEDIR/systemd && cp -r $BASEDIR/hack/package/deb/systemd $BASEDIR/
+rm -f $BASEDIR/Makefile.bak && mv $BASEDIR/Makefile $BASEDIR/Makefile.bak
+cp $BASEDIR/hack/package/deb/Makefile $BASEDIR/Makefile
 
 cd $BASEDIR
-
 debSource="$(awk -F ': ' '$1 == "Source" { print $2; exit }' debian/control)"
 debMaintainer="$(awk -F ': ' '$1 == "Maintainer" { print $2; exit }' debian/control)"
 debDate="$(date --rfc-2822)"
@@ -61,5 +42,7 @@ dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
 apt-ftparchive release ./ > Release
 
 # if you want to use your gpg key, you should replace '439AE9EC' with your key
-gpg -abs --default-key 439AE9EC -o Release.gpg Release
-gpg --clearsign --default-key 439AE9EC -o InRelease Release
+gpg -abs --default-key $DEFAULT_GPG_KEY -o Release.gpg Release
+gpg --clearsign --default-key $DEFAULT_GPG_KEY -o InRelease Release
+
+echo "Build deb package successfully! Please get the package in $PACKAGEDIR!"
