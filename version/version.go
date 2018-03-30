0ec51818e9b928bb7367587d854b7ca2530bc7c1
Michael Wan <zirenwan@gmail.com>
2018-03-29 22:42:28 -0400

    changelog: 0.3.0
    Signed-off-by: Michael Wan <zirenwan@gmail.com>

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version represents the version of pouchd.
-const Version = "0.3-dev"
+const Version = "0.3.0"
 
 // BuildTime is the time when this binary of daemon is built
 var BuildTime = "unknown"
