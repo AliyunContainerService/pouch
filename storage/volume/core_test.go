b01d3544adddf5718e5709d2444ab7e2b7a99097
Eric Li <lcy041536@gmail.com>
2018-07-05 14:44:16 +0800

    test: add unit-tests for core CreateVolume
    Signed-off-by: Eric Li <lcy041536@gmail.com>

---
+++ storage/volume/core_test.go
@@ -0,0 +1,55 @@
+package volume
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/alibaba/pouch/storage/volume/driver"
+	"github.com/alibaba/pouch/storage/volume/types"
+)
+
+func createVolumeCore(root string) (*Core, error) {
+	cfg := Config{
+		VolumeMetaPath: path.Join(root, "volume.db"),
+	}
+
+	return NewCore(cfg)
+}
+
+func TestCreateVolume(t *testing.T) {
+	volumeDriverName := "fake1"
+
+	dir, err := ioutil.TempDir("", "TestCreateVolume")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	// create volume core
+	core, err := createVolumeCore(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	driver.Register(driver.NewFakeDriver(volumeDriverName))
+	defer driver.Unregister(volumeDriverName)
+
+	v, err := core.CreateVolume(types.VolumeID{Name: "test1", Driver: volumeDriverName})
+	if err != nil {
+		t.Fatalf("create volume error: %v", err)
+	}
+
+	if v.Name != "test1" {
+		t.Fatalf("expect volume name is %s, but got %s", "test1", v.Name)
+	}
+	if v.Driver() != volumeDriverName {
+		t.Fatalf("expect volume driver is %s, but got %s", volumeDriverName, v.Driver())
+	}
+
+	_, err = core.CreateVolume(types.VolumeID{Name: "none", Driver: "none"})
+	if err == nil {
+		t.Fatal("expect get driver not found error, but err is nil")
+	}
+}
