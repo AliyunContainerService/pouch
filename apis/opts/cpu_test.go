4bc9df5257efaec5491906bd1e40d220905d23d8
{叶晓丹}{163502} <doris.yxd@alibaba-inc.com>
2018-07-09 20:05:29 +0800

    add TestValidateCPUQuota to cpu_test.go

--- apis/opts/cpu_test.go
+++ apis/opts/cpu_test.go
@@ -1,6 +1,8 @@
 package opts
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -10,4 +12,40 @@ func TestValidateCPUPeriod(t *testing.T) {
 
 func TestValidateCPUQuota(t *testing.T) {
 	// TODO
+	type TestCase struct {
+		input    int64
+		expected error
+	}
+
+	testCases := []TestCase{
+		{
+			input:    -1,
+			expected: fmt.Errorf("CPU cfs quota %d cannot be less than 1ms (i.e. 1000)", -1),
+		},
+		{
+			input:    0,
+			expected: nil,
+		},
+		{
+			input:    1000,
+			expected: nil,
+		},
+		{
+			input:    500,
+			expected: fmt.Errorf("CPU cfs quota %d cannot be less than 1ms (i.e. 1000)", 500),
+		},
+		{
+			input:    -5,
+			expected: fmt.Errorf("CPU cfs quota %d cannot be less than 1ms (i.e. 1000)", -5),
+		},
+		{
+			input:    1200,
+			expected: nil,
+		},
+	}
+
+	for _, testCase := range testCases {
+		err := ValidateCPUQuota(testCase.input)
+		assert.Equal(t, testCase.expected, err)
+	}
 }
