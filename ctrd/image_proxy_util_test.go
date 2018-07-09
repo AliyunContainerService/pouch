03e38883df154833eee7a073d2e2a9d91d64b671
chen.zgc <chen.zgc@alibaba-inc.com>
2018-07-09 21:36:41 +0800

    issues#1640

--- ctrd/image_proxy_util_test.go
+++ ctrd/image_proxy_util_test.go
@@ -1,6 +1,12 @@
 package ctrd
 
-import "testing"
+import (
+	"fmt"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestHasPort(t *testing.T) {
 	// TODO
@@ -8,8 +14,39 @@ func TestHasPort(t *testing.T) {
 
 func TestCanonicalAddr(t *testing.T) {
 	// TODO
+	type TestCase struct {
+		input    *url.URL
+		expected string
+	}
+
+	s1 := "postgres://user:pass@host.com:5432/path?k=v#f"
+	u1, err1 := url.Parse(s1)
+	if err1 != nil {
+		fmt.Println(u1)
+	}
+
+	s2 := "https://user:pass@host.com:5432/path?k=v#f"
+	u2, err2 := url.Parse(s2)
+	if err2 != nil {
+		fmt.Println(u2)
+	}
+
+	testCases := []TestCase{
+		{
+			input:    u1,
+			expected: "postgres:port",
+		},
+		{
+			input:    u2,
+			expected: "https:port",
+		},
+	}
 }
 
 func TestUseProxy(t *testing.T) {
 	// TODO
+	for _, testCase := range testCases {
+		output := canonicalAddr(testCase.input)
+		assert.Equal(t, testCase.expected, output)
+	}
 }
