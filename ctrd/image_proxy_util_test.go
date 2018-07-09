5dac0fbaff90a1f5abeec97c71742b70c9f7f552
huan <huan@huandeMacBook-Air.local>
2018-07-09 20:16:40 +0800

    test: cpu, oom-score, image-proxy, config
    Signed-off-by: huan <hliang47@wisc.edu>

--- ctrd/image_proxy_util_test.go
+++ ctrd/image_proxy_util_test.go
@@ -1,41 +1,93 @@
 package ctrd
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"net/url"
+	"os"
+	"testing"
+)
 
-func TestHasPort(t *testing.T) {
+func TestCanonicalAddr(t *testing.T) {
 	type args struct {
-		str string
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		// TODO: Add test cases.
-		{name: "test1", args: args{str: string("localhost:8000")}, want: true},
-		{name: "test2", args: args{str: string("[ipv6::localhost]:8000")}, want: true},
-		{name: "test3", args: args{str: string(":8000")}, want: true},
-		{name: "test4", args: args{str: string("[ipv6::127.0.0.1]::8000")}, want: true},
-		{name: "test5", args: args{str: string("localhost")}, want: false},
-		{name: "test6", args: args{str: string("[ipv6::localhost]")}, want: false},
-		{name: "test7", args: args{str: string("[ipv6::localhost]8000")}, want: false},
+		url *url.URL
 	}
 
+	var user1 = url.User("felix")
+	var url1 = url.URL{Scheme: "http", Opaque: "", User: user1, Host: "127.0.0.1", Path: "/", RawPath: "", ForceQuery: false, RawQuery: "", Fragment: ""}
+	var url2 = url.URL{Scheme: "https", Opaque: "", User: user1, Host: "127.0.0.1", Path: "/", RawPath: "", ForceQuery: false, RawQuery: "", Fragment: ""}
+	var url3 = url.URL{Scheme: "socks5", Opaque: "", User: user1, Host: "127.0.0.1", Path: "/", RawPath: "", ForceQuery: false, RawQuery: "", Fragment: ""}
+	var url4 = url.URL{Scheme: "socks5", Opaque: "", User: user1, Host: "127.0.0.1:22", Path: "/", RawPath: "", ForceQuery: false, RawQuery: "", Fragment: ""}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr string
+	}{
+		{name: "test1", args: args{url: &url1}, wantErr: "127.0.0.1:80"},
+		{name: "test2", args: args{url: &url2}, wantErr: "127.0.0.1:443"},
+		{name: "test3", args: args{url: &url3}, wantErr: "127.0.0.1:1080"},
+		{name: "test4", args: args{url: &url4}, wantErr: "127.0.0.1:22"},
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := hasPort(tt.args.str)
-			if got != tt.want {
-				t.Errorf("hasPort() = %v, want %v", got, tt.want)
-				return
-			}
+			err := canonicalAddr(tt.args.url)
+			assert.Equal(t, tt.wantErr, err)
+
 		})
 	}
 }
 
-func TestCanonicalAddr(t *testing.T) {
-	// TODO
+func TestHasPort(t *testing.T) {
+	type args struct {
+		s string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+
+		{name: "test1", args: args{s: "127.0.0.1"}, wantErr: false},
+		{name: "test2", args: args{s: "127.0.0.1:80"}, wantErr: true},
+		{name: "test3", args: args{s: "[ FF01::1101]:80"}, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := hasPort(tt.args.s)
+			assert.Equal(t, tt.wantErr, err)
+
+		})
+	}
 }
 
 func TestUseProxy(t *testing.T) {
-	// TODO
+
+	os.Setenv("NO_PROXY", "www.taobao.com:80,www.taobao1.com:80")
+	noProxyEnv.init()
+
+	type args struct {
+		addr string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+
+		{name: "test1", args: args{addr: ""}, wantErr: true},
+		{name: "test1", args: args{addr: "localhost:80"}, wantErr: false},
+		{name: "test1", args: args{addr: "127.0.0.1:80"}, wantErr: false},
+		{name: "test1", args: args{addr: "www.taobao.com:80"}, wantErr: false},
+		{name: "test1", args: args{addr: "www.taobao1.com:80"}, wantErr: false},
+		{name: "test1", args: args{addr: "www.taobao2.com:80"}, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := useProxy(tt.args.addr)
+			assert.Equal(t, tt.wantErr, err)
+
+		})
+	}
 }
--- daemon/config/config_test.go
+++ daemon/config/config_test.go
@@ -1,9 +1,10 @@
 package config
 
 import (
-	"testing"
-
+	"fmt"
+	"github.com/alibaba/pouch/apis/types"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestIterateConfig(t *testing.T) {
@@ -44,14 +45,44 @@ func TestIterateConfig(t *testing.T) {
 	assert.Equal(config, map[string]interface{}{})
 }
 
-func TestConfigValidate(t *testing.T) {
-	// TODO
-}
+func TestValidate(t *testing.T) {
+	type args struct {
+		cfg *Config
+	}
+	//all complete
+	var cfg1 Config
+	cfg1.Labels = []string{"key=value"}
+	// len != 2
+	var cfg2 Config
+	cfg2.Labels = []string{"key"}
+	// key empty
+	var cfg3 Config
+	cfg3.Labels = []string{"=value"}
+	// value empty
+	var cfg4 Config
+	cfg4.Labels = []string{"key="}
+	// test runtime
+	var cfg5 Config
+	cfg5.DefaultRuntime = "test"
+	cfg5.Runtimes = map[string]types.Runtime{"test": {"path", []string{"runargs"}}}
 
-func TestGetConflictConfigurations(t *testing.T) {
-	// TODO
-}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr error
+	}{
+		{name: "test1", args: args{cfg: &cfg1}, wantErr: nil},
+		{name: "test2", args: args{cfg: &cfg2}, wantErr: fmt.Errorf("daemon label %s must be in format of key=value", "key")},
+		{name: "test3", args: args{cfg: &cfg3}, wantErr: fmt.Errorf("key and value in daemon label %s cannot be empty", "=value")},
+		{name: "test4", args: args{cfg: &cfg4}, wantErr: fmt.Errorf("key and value in daemon label %s cannot be empty", "key=")},
+		{name: "test5", args: args{cfg: &cfg5}, wantErr: fmt.Errorf("default runtime %s cannot be re-register", "test")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.args.cfg.Validate()
+			assert.Equal(t, tt.wantErr, err)
 
-func TestGetUnknownFlags(t *testing.T) {
-	// TODO
+		})
+	}
+
 }
