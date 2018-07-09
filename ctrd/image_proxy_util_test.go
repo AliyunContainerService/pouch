088f2c7dfddc249d1d59b40bfe5336a806e0879f
mengjiahao <691225916@qq.com>
2018-07-10 02:35:46 +0800

    fix not use underscores name

--- ctrd/image_proxy_util_test.go
+++ ctrd/image_proxy_util_test.go
@@ -4,26 +4,26 @@ import "testing"
 
 func TestHasPort(t *testing.T) {
 	s := "127.0.0.1:80"
-	has_port := hasPort(s)
-	if has_port != true {
+	has := hasPort(s)
+	if has != true {
 		t.Fatalf("expect host:port %s has port, but return false", s)
 	}
 
 	s = "[FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF::127.0.0.1]:80"
-	has_port = hasPort(s)
-	if has_port != true {
+	has = hasPort(s)
+	if has != true {
 		t.Fatalf("expect [ipv6::address]:port %s has port, but return false", s)
 	}
 
 	s = "127.0.0.1"
-	has_port = hasPort(s)
-	if has_port == true {
+	has = hasPort(s)
+	if has == true {
 		t.Fatalf("expect host %s has not port, but return true", s)
 	}
 
 	s = "[FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF::127.0.0.1]"
-	has_port = hasPort(s)
-	if has_port == true {
+	has = hasPort(s)
+	if has == true {
 		t.Fatalf("expect [ipv6::address]:port %s has not port, but return true", s)
 	}
 }
