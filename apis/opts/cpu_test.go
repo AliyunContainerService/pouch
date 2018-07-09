78e318cd3a6b5e5c417a0dba4e8490aca68ef292
jiahao.mjh <jiahao.mjh@alibaba-inc.com>
2018-07-09 20:04:05 +0800

    fix ValidateCPUPeroid

--- apis/opts/cpu_test.go
+++ apis/opts/cpu_test.go
@@ -1,11 +1,34 @@
 package opts
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestValidateCPUPeriod(t *testing.T) {
-	// TODO
+	rand.Seed(time.Now().Unix())
+
+	err := ValidateCPUPeriod(0)
+	if err != nil {
+		t.Fatal("validate cpu peroid 0 err: %v", err)
+	}
+
+	for i := 0; i < 100; i++ {
+		p := rand.Int63n(10000000)
+		p -= 1000
+		if 1000 <= p && p <= 1000000 {
+			err = ValidateCPUPeriod(p)
+			if err != nil {
+				t.Fatal("validate cpu peroid %v err: %v", p, err)
+			}
+		} else {
+			err = ValidateCPUPeriod(p)
+			if err == nil {
+				t.Fatal("expect validate cpu peroid %v error, but err is nil", p)
+			}
+		}
+	}
 }
 
 func TestValidateCPUQuota(t *testing.T) {
