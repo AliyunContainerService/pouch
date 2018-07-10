841d993ca7690341fe815b570012e1d99b966895
mengjiahao <691225916@qq.com>
2018-07-10 09:58:37 +0800

    test: TestRemoveVolume[C

--- storage/volume/core_test.go
+++ storage/volume/core_test.go
@@ -67,7 +67,44 @@ func TestListVolumeName(t *testing.T) {
 }
 
 func TestRemoveVolume(t *testing.T) {
-	// TODO
+	volName1 := "vol2"
+	driverName1 := "fake_driver12"
+	volid1 := types.VolumeID{Name: volName1, Driver: driverName1}
+
+	dir, err := ioutil.TempDir("", "TestGetVolume")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	core, err := createVolumeCore(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	driver.Register(driver.NewFakeDriver(driverName1))
+	defer driver.Unregister(driverName1)
+
+	v1, err1 := core.CreateVolume(volid1)
+	if err != nil {
+		t.Fatalf("create volume error: %v", err1)
+	}
+	if v1.Name != volName1 {
+		t.Fatalf("expect volume name is %s, but got %s", volName1, v1.Name)
+	}
+	if v1.Driver() != driverName1 {
+		t.Fatalf("expect volume driver is %s, but got %s", driverName1, v1.Driver())
+	}
+
+	err2 := core.RemoveVolume(volid1)
+	if err2 != nil {
+		t.Fatalf("remove volume id %v error: %v", volid1, err2)
+	}
+
+	err3 := core.RemoveVolume(volid1)
+	if err3 == nil {
+		t.Fatalf("expect remove empty volume id %v error, but return nil", volid1)
+	}
 }
 
 func TestVolumePath(t *testing.T) {
