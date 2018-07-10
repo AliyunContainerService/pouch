c2bcf7708c77054b521fee0514141a3e21158e4f
“筱媱” <mengna.ymn@alibaba-inc.com>
2018-07-10 09:36:32 +0800

    add unit test for TestValidateCPUQuota

--- apis/opts/cpu_test.go
+++ apis/opts/cpu_test.go
@@ -11,7 +11,6 @@ func TestValidateCPUPeriod(t *testing.T) {
 		period  int64
 		wantErr bool
 	}{
-		// TODO: Add test cases.
 		{name: "test1", period: 0, wantErr: false},
 		{name: "test2", period: 10, wantErr: true},
 		{name: "test3", period: 2000000, wantErr: true},
@@ -21,6 +20,27 @@ func TestValidateCPUPeriod(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := ValidateCPUPeriod(tt.period); (err != nil) != tt.wantErr {
+				t.Errorf("ValidateCPUPeriod() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestValidateCPUQuota(t *testing.T) {
+	cases := []struct{
+		name    string
+		in      int64
+		wantErr  bool
+	}{
+		{name: "test1", in: 0, wantErr: false},
+		{name: "test2", in: 1000, wantErr: false},
+		{name: "test3", in: 2000, wantErr: false},
+		{name: "test4", in: 500, wantErr: true},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := ValidateCPUQuota(tt.in); (err != nil) != tt.wantErr {
 				t.Errorf("ValidateCPUPeriod() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
