6a76982bbb46a050fa77b573370ec898b3935ff2
mengjiahao <691225916@qq.com>
2018-07-10 11:59:29 +0800

    test: add unit-test for proxy/has port (#1668)
    add unit test for function hasPort

--- ctrd/image_proxy_util_test.go
+++ ctrd/image_proxy_util_test.go
@@ -3,7 +3,29 @@ package ctrd
 import "testing"
 
 func TestHasPort(t *testing.T) {
-	// TODO
+	s := "127.0.0.1:80"
+	has := hasPort(s)
+	if has != true {
+		t.Fatalf("expect host:port %s has port, but return false", s)
+	}
+
+	s = "[FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF::127.0.0.1]:80"
+	has = hasPort(s)
+	if has != true {
+		t.Fatalf("expect [ipv6::address]:port %s has port, but return false", s)
+	}
+
+	s = "127.0.0.1"
+	has = hasPort(s)
+	if has == true {
+		t.Fatalf("expect host %s has not port, but return true", s)
+	}
+
+	s = "[FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF::127.0.0.1]"
+	has = hasPort(s)
+	if has == true {
+		t.Fatalf("expect [ipv6::address]:port %s has not port, but return true", s)
+	}
 }
 
 func TestCanonicalAddr(t *testing.T) {
