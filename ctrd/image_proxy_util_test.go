baed353bccde49ca1ae5e7e09b4f765396e898c3
haokun.yhk <kungaroo931105@gmail.com>
2018-07-10 11:59:13 +0800

    m

--- ctrd/image_proxy_util_test.go
+++ ctrd/image_proxy_util_test.go
@@ -5,8 +5,26 @@ import (
 	"testing"
 )
 
-func TestHasPort(t *testing.T) {
-	// TODO
+func TestUseProxy(t *testing.T) {
+	tests := []struct {
+		name string
+		addr string
+		want bool
+	}{
+		{addr: string("192.168.1.1:80"), want: true},
+		{addr: string("192.168.1.2:80"), want: true},
+		{addr: string("192.168.1.3:80"), want: true},
+		{addr: string("192.168.1.4:80"), want: true},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := useProxy(test.addr)
+			if got == false {
+				t.Errorf("useProxy(%v) = %v, want %v", test.addr, got, test.want)
+				return
+			}
+		})
+	}
 }
 
 func TestCanonicalAddr(t *testing.T) {
