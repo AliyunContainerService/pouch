b9ac1966a3e3e05e8aeb6ac6fab1520ed45f9a5e
lize.yc@alibaba-inc.com <faremax@aliyun.com>
2018-07-10 10:48:06 +0800

    fix bugs

--- apis/opts/cpu_test.go
+++ apis/opts/cpu_test.go
@@ -14,12 +14,12 @@ func TestValidateCPUQuota(t *testing.T) {
 		t.Fatalf("Wrong return:expected nil,actual %v", result)
 	}
 
-	result := ValidateCPUQuota(1)
+	result = ValidateCPUQuota(1)
 	if result == nil {
 		t.Fatal("illegal period error is expected,but err is nil")
 	}
 
-	result := ValidateCPUQuota(1001)
+	result = ValidateCPUQuota(1001)
 	if result != nil {
 		t.Fatalf("Wrong return:expected nil,actual %v", result)
 	}
