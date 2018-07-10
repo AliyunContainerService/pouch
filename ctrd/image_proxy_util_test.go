3f989a817b18213d1114b8eeb1e6371ac4421541
lize.yc@alibaba-inc.com <faremax@aliyun.com>
2018-07-10 11:45:54 +0800

    cannonialaddr

--- ctrd/image_proxy_util_test.go
+++ ctrd/image_proxy_util_test.go
@@ -7,7 +7,46 @@ func TestHasPort(t *testing.T) {
 }
 
 func TestCanonicalAddr(t *testing.T) {
-	// TODO
+	url := "http://localhost:9090/api/download?fileidstr=2d905c2162b50d0e12b2213323b34bd4.xlsx&iswindows=0&optuser=test"
+
+	addr := canonicalAddr(url)
+
+	if addr == nil {
+		t.Fatal("returned value is expected,but is nil")
+	}
+	if addr != "localhost:9090" {
+		t.Fatal("Wrong return:expected localhost:9090,actual ", addr)
+	}
+
+	url = "http://localhost/api/download?fileidstr=2d905c2162b50d0e12b2213323b34bd4.xlsx&iswindows=0&optuser=test"
+	addr = canonicalAddr(url)
+
+	if addr == nil {
+		t.Fatal(returned value is expected,but is nil)
+	}
+	if addr != "localhost:80" {
+		t.Fatal("Wrong return:expected localhost:9090,actual ", addr)
+	}
+
+	url = "https://localhost/api/download?fileidstr=2d905c2162b50d0e12b2213323b34bd4.xlsx&iswindows=0&optuser=test"
+	addr = canonicalAddr(url)
+
+	if addr == nil {
+		t.Fatal(returned value is expected,but is nil)
+	}
+	if addr != "localhost:443" {
+		t.Fatal("Wrong return:expected localhost:9090,actual ", addr)
+	}
+
+	url = "socks5://localhost/api/download?fileidstr=2d905c2162b50d0e12b2213323b34bd4.xlsx&iswindows=0&optuser=test"
+	addr = canonicalAddr(url)
+
+	if addr == nil {
+		t.Fatal(returned value is expected,but is nil)
+	}
+	if addr != "localhost:1080" {
+		t.Fatal("Wrong return:expected localhost:9090,actual ", addr)
+	}
 }
 
 func TestUseProxy(t *testing.T) {
