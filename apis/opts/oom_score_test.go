bfdc81f9d7e0bd7b1dab05c3a4a9d08eb783b2ba
夏顺兴 <shunxing.xsx@alibaba-inc.com>
2018-07-10 09:18:59 +0800

    oom_score_test.go

--- apis/opts/oom_score_test.go
+++ apis/opts/oom_score_test.go
@@ -1,7 +1,39 @@
 package opts
 
-import "testing"
+import (
+	"testing"
+	"math/rand"
+	"time"
+)
 
-func TestValidateOOMScore(t *testing.T) {
-	// TODO
+func TestValidateOOMScoreSucceed(t *testing.T) {
+	num1 := int(2000)
+	num2 := int(1000)
+
+	var i int
+	for i = 0; i < 5; i++ {
+		r := rand.New(rand.NewSource(time.Now().Unix()))
+		e := ValidateOOMScore(int64(r.Intn(num1) - num2))
+		if e != nil {
+			t.Error("test failed")
+			return
+		}
+	}
+}
+
+func TestValidateOOMScoreFailed(t *testing.T) {
+	uvinf := 0x7FF00000000000
+	uvneginf := 0xFFF00000000000
+	num1 := int(1000)
+
+	var j int
+	for j = 0; j < 5; j++ {
+		r := rand.New(rand.NewSource(time.Now().Unix()))
+		e := ValidateOOMScore(int64(uvinf - r.Intn(num1)))
+		eNew := ValidateOOMScore(- int64(uvneginf - r.Intn(num1)))
+		if e != nil || eNew != nil{
+			t.Error("test failed")
+			return
+		}
+	}
 }
