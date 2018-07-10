377d4eda478f911a4a6cbcf2741ede108e6a41a4
lize.yc@alibaba-inc.com <faremax@aliyun.com>
2018-07-10 11:43:27 +0800

    fix

--- ctrd/image_proxy_util_test.go
+++ ctrd/image_proxy_util_test.go
@@ -3,11 +3,34 @@ package ctrd
 import "testing"
 
 func TestHasPort(t *testing.T) {
-	// TODO
+	s := "172.0.0.1"
+	result := hasPort(s)
+	if result == true {
+		t.Fatalf("Wrong return:expected false,actual %v", result)
+	}
+
+	s = "172.0.0.1:22"
+	result = hasPort(s)
+	if result == false {
+		t.Fatalf("Wrong return:expected true,actual %v", result)
+	}
+
+	s = "[ipv6:172.0.0.1]:22"
+	result = hasPort(s)
+	if result == false {
+		t.Fatalf("Wrong return:expected true,actual %v", result)
+	}
+
+	s = "[ipv6:172.0.0.1]"
+	result = hasPort(s)
+	if result == true {
+		t.Fatalf("Wrong return:expected false,actual %v", result)
+	}
 }
 
 func TestCanonicalAddr(t *testing.T) {
 	// TODO
+
 }
 
 func TestUseProxy(t *testing.T) {
