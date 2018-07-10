16ea33f089ba110678609919d42c51a7269548e7
xiyu.wxy <xiyu.wxy@alibaba-inc.com>
2018-07-10 09:27:35 +0800

    add tests for storage/volume

--- storage/volume/driver/fake_driver.go
+++ storage/volume/driver/fake_driver.go
@@ -1,6 +1,7 @@
 package driver
 
 import (
+	volerr "github.com/alibaba/pouch/storage/volume/error"
 	"path"
 
 	"github.com/alibaba/pouch/storage/volume/types"
@@ -44,4 +45,38 @@ func (f *FakeDriver) Remove(ctx Context, volume *types.Volume) error {
 // Path returns fake volume's path.
 func (f *FakeDriver) Path(ctx Context, volume *types.Volume) (string, error) {
 	return path.Join("/fake", volume.Name), nil
+}
+
+// Get a volume from fake driver.
+func (f *FakeDriver) Get(ctx Context, name string) (*types.Volume, error) {
+
+	if name == "test1" {
+		id := types.NewVolumeID(name, f.Name(ctx))
+		mountPath := path.Join("/fake", id.Name)
+		return types.NewVolumeFromID(mountPath, "", id), nil
+	}
+	return nil, volerr.ErrVolumeNotFound
+
+}
+
+// List all volumes from fake driver.
+func (f *FakeDriver) List(ctx Context) ([]*types.Volume, error) {
+	var vList []*types.Volume
+
+	id := types.NewVolumeID("fake2", f.Name(ctx))
+	mountPath := path.Join("/fake", id.Name)
+	volume := types.NewVolumeFromID(mountPath, "", id)
+	vList = append(vList, volume)
+
+	return vList, nil
+}
+
+// Attach a fake volume.
+func (f *FakeDriver) Attach(ctx Context, v *types.Volume) error {
+	return nil
+}
+
+// Detach a fake volume.
+func (f *FakeDriver) Detach(ctx Context, v *types.Volume) error {
+	return nil
 }
