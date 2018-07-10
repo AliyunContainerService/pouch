d6f8a2bee8f7fff116e441b581d04ed2d4e43fdc
Allen Sun <shlallen1990@gmail.com>
2018-07-10 12:11:54 +0800

    Revert "test: add unit-test for proxy/has port (#1668)"
    This reverts commit 6a76982bbb46a050fa77b573370ec898b3935ff2.

--- ctrd/image_proxy_util_test.go
+++ ctrd/image_proxy_util_test.go
@@ -3,29 +3,7 @@ package ctrd
 import "testing"
 
 func TestHasPort(t *testing.T) {
-	s := "127.0.0.1:80"
-	has := hasPort(s)
-	if has != true {
-		t.Fatalf("expect host:port %s has port, but return false", s)
-	}
-
-	s = "[FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF::127.0.0.1]:80"
-	has = hasPort(s)
-	if has != true {
-		t.Fatalf("expect [ipv6::address]:port %s has port, but return false", s)
-	}
-
-	s = "127.0.0.1"
-	has = hasPort(s)
-	if has == true {
-		t.Fatalf("expect host %s has not port, but return true", s)
-	}
-
-	s = "[FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF::127.0.0.1]"
-	has = hasPort(s)
-	if has == true {
-		t.Fatalf("expect [ipv6::address]:port %s has not port, but return true", s)
-	}
+	// TODO
 }
 
 func TestCanonicalAddr(t *testing.T) {
