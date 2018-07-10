5c1ea2f86d14798b419ddd986db97d122ea4b515
夏顺兴 <shunxing.xsx@alibaba-inc.com>
2018-07-10 10:07:18 +0800

    oom_score_test.go

--- apis/opts/oom_score_test.go
+++ apis/opts/oom_score_test.go
@@ -2,38 +2,33 @@ package opts
 
 import (
 	"testing"
-	"math/rand"
-	"time"
 )
 
-func TestValidateOOMScoreSucceed(t *testing.T) {
-	num1 := int(2000)
-	num2 := int(1000)
-
-	var i int
-	for i = 0; i < 5; i++ {
-		r := rand.New(rand.NewSource(time.Now().Unix()))
-		e := ValidateOOMScore(int64(r.Intn(num1) - num2))
-		if e != nil {
-			t.Error("test failed")
-			return
-		}
+func TestValidateOOMScore(t *testing.T) {
+	cases := []struct {
+		name string
+		score int64
+		want bool
+	}{
+		{"test1", 0, false},
+		{"test2", 10, false},
+		{"test3", 100, false},
+		{"test4", 1000, false},
+		{"test5", -10, false},
+		{"test6", -100, false},
+		{"test7", -1000, false},
+		{"test8", 2000, true},
+		{"test9", 5000, true},
+		{"test10", -2000, true},
+		{"test11", -5000, true},
 	}
-}
 
-func TestValidateOOMScoreFailed(t *testing.T) {
-	uvinf := 0x7FF00000000000
-	uvneginf := 0xFFF00000000000
-	num1 := int(1000)
-
-	var j int
-	for j = 0; j < 5; j++ {
-		r := rand.New(rand.NewSource(time.Now().Unix()))
-		e := ValidateOOMScore(int64(uvinf - r.Intn(num1)))
-		eNew := ValidateOOMScore(- int64(uvneginf - r.Intn(num1)))
-		if e != nil || eNew != nil{
-			t.Error("test failed")
-			return
-		}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := ValidateOOMScore(c.score)
+			if got != nil && (got != nil) != c.want{
+				t.Error("test failed")
+			}
+		})
 	}
 }
