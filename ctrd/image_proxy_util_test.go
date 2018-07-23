e119110fa19531e91a3172def72b01a4119c8813
highspeedbuck <tanjunlang@qq.com>
2018-07-24 04:15:37 +0800

    fix issues#1760

--- ctrd/image_proxy_util_test.go
+++ ctrd/image_proxy_util_test.go
@@ -1,6 +1,9 @@
 package ctrd
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 func TestHasPort(t *testing.T) {
 	type args struct {
@@ -38,4 +41,72 @@ func TestCanonicalAddr(t *testing.T) {
 
 func TestUseProxy(t *testing.T) {
 	// TODO
+
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{name: "FormatError", args: args{str: string("352.242.123.312::8080")}, want: false},
+		{name: "StringIsNull", args: args{str: string("")}, want: true},
+		{name: "localhost", args: args{str: string("localhost:8000")}, want: false},
+		{name: "isLoopbak", args: args{str: string("127.0.0.1:8000")}, want: false},
+		{name: "ipv6Localhost", args: args{str: string("[ipv6::localhost]:8000")}, want: true},
+		{name: "ipv6IsNUll", args: args{str: string("[ipv6::127.0.0.1]:8000")}, want: true},
+		{name: "ipv6FormatError", args: args{str: string("[ipv6::127.0.0.1]::8000")}, want: false},
+		{name: "ipv6http", args: args{str: string("[ipv6::127.0.0.1]:http")}, want: true},
+		{name: "missHost", args: args{str: string(":8000")}, want: true},
+		{name: "missPort", args: args{str: string("localhost")}, want: false},
+		{name: "ipv6missPort", args: args{str: string("[ipv6::localhost]")}, want: false},
+		{name: "ipv6missSemicolon", args: args{str: string("[ipv6::localhost]8000")}, want: false},
+		{name: "normalPass", args: args{str: string("354.172.12.1:8080")}, want: true},
+		{name: "matchEnv1", args: args{str: string("bar.foo.com:8080")}, want: false},
+		{name: "matchEnv2", args: args{str: string("foo.com:8080")}, want: false},
+		{name: "matchEnv3", args: args{str: string("bar.foo.com:8080")}, want: false},
+		{name: "notMatchEnv", args: args{str: string("www.alibaba.com:8080")}, want: true},
+		{name: "allFalseEnv", args: args{str: string("localhost:8080")}, want: false},
+
+	}
+	env := []struct{
+		name string
+		val string
+	}{
+		//The environment variable for testCast	
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: ".foo.com"},
+		{name: "NO_PROXY", val: "foo.com"},
+		{name: "NO_PROXY", val: "foo.com,.foo.com,baidu.com"},
+		{name: "NO_PROXY", val: "*"},
+	}
+
+	count := 0
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Setenv(env[count].name, env[count].val)
+			count = count + 1
+			got := useProxy(tt.args.str)
+			if got != tt.want {
+				t.Errorf("useProxy() = %v, want %v", got, tt.want)
+				return
+			}
+		})
+	}
 }
