99980e65704f1d75f375fa518b5051d39bfc0c3f
尘逸 <shiqiang.zsq@alibaba-inc.com>
2018-07-23 21:56:40 +0800

    add image_proxy_util test

--- ctrd/image_proxy_util_test.go
+++ ctrd/image_proxy_util_test.go
@@ -1,6 +1,9 @@
 package ctrd
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func TestHasPort(t *testing.T) {
 	type args struct {
@@ -36,6 +39,48 @@ func TestCanonicalAddr(t *testing.T) {
 	// TODO
 }
 
+var (
+	noProxyEnv = &envOnce{
+		names: []string{"NO_PROXY", "no_proxy"},
+	}
+)
+
 func TestUseProxy(t *testing.T) {
 	// TODO
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{name: "test1", args: args{str: string("")}, want: true},
+		{name: "test2", args: args{str: string("127.0.0.1")}, want: false},
+		{name: "test3", args: args{str: string("[127.0.0.1")}, want: false},
+		{name: "test4", args: args{str: string("[127.0.0.1]")}, want: false},
+		{name: "test5", args: args{str: string("[127.0.0.1]8080")}, want: false},
+		{name: "test6", args: args{str: string("[127.0.0.1]::8080")}, want: false},
+		{name: "test7", args: args{str: string("127.0.0.1::123")}, want: false},
+		{name: "test8", args: args{str: string("[[127.0.0.1]:8080")}, want: false},
+		{name: "test9", args: args{str: string("[127.0.0.1]]:8080")}, want: false},
+		{name: "test10", args: args{str: string("localhost:8080")}, want: false},
+		{name: "test11", args: args{str: string("localhost:8080")}, want: false},
+		{name: "test12", args: args{str: string("localhost:8080")}, want: false},
+	}
+
+	noProxy := noProxyEnv.Get()
+
+	fmt.Println(noProxy)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := useProxy(tt.args.str)
+			if got != tt.want {
+				t.Errorf("useProxy() = %v, want %v", got, tt.want)
+				return
+			}
+		})
+	}
 }
