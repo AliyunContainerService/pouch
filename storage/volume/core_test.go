5fc7633e887ea3e238d2f677f369847be96ee3d2
btlcmr0702 <btlcmr0702@hotmail.com>
2018-07-24 13:53:42 +0800

    test: add unit test for func storage/volume/DetachVolume
    Signed-off-by: btlcmr0702 <btlcmr0702@hotmail.com>

--- storage/volume/core_test.go
+++ storage/volume/core_test.go
@@ -176,7 +176,7 @@ func TestListVolumes(t *testing.T) {
 }
 
 func TestListVolumeName(t *testing.T) {
-	//TODO
+	// TODO
 }
 
 func TestRemoveVolume(t *testing.T) {
@@ -221,52 +221,7 @@ func TestRemoveVolume(t *testing.T) {
 }
 
 func TestVolumePath(t *testing.T) {
-	volName1 := "myVol"
-	driverName1 := "my_fake_driver"
-	volid1 := types.VolumeID{Name: volName1, Driver: driverName1}
-
-	dir, err := ioutil.TempDir("", "TestGetVolume")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(dir)
-
-	core, errCvc := createVolumeCore(dir)
-	if errCvc != nil {
-		t.Fatal(err)
-	}
-	volumePath1, errVp := core.VolumePath(volid1)
-	if errVp == nil {
-		t.Fatalf("volume path of volume %v should be nil!", volid1)
-	}
-
-	driver.Register(driver.NewFakeDriver(driverName1))
-	defer driver.Unregister(driverName1)
-
-	v1, errCv := core.CreateVolume(volid1)
-	if errCv != nil {
-		t.Fatalf("create volume %v fail, error %v", volid1, errCv)
-	}
-	if v1.Name != volName1 {
-		t.Fatalf("volume name should be %s not %s", volName1, v1.Name)
-	}
-	if v1.Driver() != driverName1 {
-		t.Fatalf("volume driver should be %s not %s", driverName1, v1.Driver())
-	}
-	_, errGv := core.GetVolume(volid1)
-	if errGv != nil {
-		t.Fatalf("get volume %v fail, error %v", volid1, errGv)
-	}
-
-	volumePath2, errVp2 := core.VolumePath(volid1)
-	if errVp2 != nil {
-		t.Fatalf("volume path of volume %v error", volid1)
-	}
-
-	if volumePath1 == volumePath2 {
-		t.Fatalf("volume path %s should not be nil", volumePath2)
-	}
-
+	// TODO
 }
 
 func TestAttachVolume(t *testing.T) {
@@ -274,5 +229,42 @@ func TestAttachVolume(t *testing.T) {
 }
 
 func TestDetachVolume(t *testing.T) {
-	// TODO
+	var volumeName = "testDetachVolume"
+	volumeDriverName := "fakeTestDetachVolume"
+	dir, err := ioutil.TempDir("", "TestDetachVolume")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	core, errCvc := createVolumeCore(dir)
+	if errCvc != nil {
+		t.Fatal("create volume core fail")
+	}
+
+	driver.Register(driver.NewFakeDriver(volumeDriverName))
+	defer driver.Unregister(volumeDriverName)
+
+	volid := types.VolumeID{Name: volumeName, Driver: volumeDriverName}
+	_, errCv := core.CreateVolume(volid)
+	if errCv != nil {
+		t.Fatalf("create volume %v fail, error %v", volid, errCv)
+	}
+
+	var extra map[string]string
+	_, errAv := core.AttachVolume(volid, extra)
+	if errAv != nil {
+		t.Fatalf("attatch volume fail, error %v", errAv)
+	}
+	v, errDv := core.DetachVolume(volid, extra)
+	if errDv != nil {
+		t.Fatalf("detach volume fail, error %v", errDv)
+	}
+
+	if v.Name != volumeName {
+		t.Fatalf("volume name should be %s, but it is %s", volumeName, v.Name)
+	}
+	if v.Driver() != volumeDriverName {
+		t.Fatalf("volume driver should be %s, but it is %s", volumeDriverName, v.Driver())
+	}
 }
