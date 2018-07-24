f302d5fd3392def44a5479500751a460735ca27c
zh6666 <zhouhang6363@163.com>
2018-07-24 09:51:04 +0800

    issue 1 2

--- daemon/config/config_test.go
+++ daemon/config/config_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/spf13/pflag"
 )
 
 func TestIterateConfig(t *testing.T) {
@@ -41,6 +42,7 @@ func TestIterateConfig(t *testing.T) {
 	// test nil map will not cause panic
 	config = make(map[string]interface{})
 	iterateConfig(nil, config)
+
 	assert.Equal(config, map[string]interface{}{})
 }
 
@@ -49,9 +51,59 @@ func TestConfigValidate(t *testing.T) {
 }
 
 func TestGetConflictConfigurations(t *testing.T) {
-	// TODO
+	//without  conflict
+	flagSet0 := pflag.NewFlagSet("flagset1", 0)
+	flagSet0.String("a", "1", "test")
+	s0 := []string{"--a", "31111"}
+	flagSet0.Parse(s0)
+	//with slice conflict
+	flagSet1 := pflag.NewFlagSet("flagset1", 0)
+	flagSet1.StringSlice("slice", []string{"111", "222", "333"}, "test")
+	s1 := []string{"--slice=aaa,bbb,ccc"}
+	flagSet1.Parse(s1)
+	//with conflict
+	flagSet2 := pflag.NewFlagSet("flagset2", 0)
+	flagSet2.String("b", "3", "test")
+	s2 := []string{"--b", "222"}
+	flagSet2.Parse(s2)
+	fileFlags := map[string]interface{}{
+		"b":     "2",
+		"slice": []string{"111", "222", "333"},
+	}
+	error := getConflictConfigurations(flagSet0, fileFlags)
+	if error != nil {
+		t.Fatal(error)
+	}
+	error = getConflictConfigurations(flagSet1, fileFlags)
+	if error != nil {
+		t.Fatal(error)
+	}
+	error = getConflictConfigurations(flagSet2, fileFlags)
+	if error == nil {
+		t.Fatal("expect get driver not found error, but err is nil")
+	}
 }
 
 func TestGetUnknownFlags(t *testing.T) {
-	// TODO
+
+	//without unknown flags
+	flagSet := pflag.NewFlagSet("flagset1", 0)
+	flagSet.String("a", "1", "test")
+	flagSet.String("b", "2", "test")
+	//with unknown flags
+	flagSet2 := pflag.NewFlagSet("flagset2", 0)
+	flagSet2.String("a", "1", "test")
+	flagSet2.String("c", "2", "test")
+	fileFlags := map[string]interface{}{
+		"a": "1",
+		"b": "2",
+	}
+	error := getUnknownFlags(flagSet, fileFlags)
+	if error != nil {
+		t.Fatal(error)
+	}
+	error = getUnknownFlags(flagSet2, fileFlags)
+	if error == nil {
+		t.Fatal("expect get driver not found error, but err is nil")
+	}
 }
