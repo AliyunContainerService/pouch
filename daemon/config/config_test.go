679e417c8ceeba891a90d905927c981dbb820143
yannanyue <2682463492@qq.com>
2018-07-24 10:40:20 +0800

    fixes#1758 baiji-group=264-3

--- daemon/config/config_test.go
+++ daemon/config/config_test.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/spf13/pflag"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -53,5 +55,31 @@ func TestGetConflictConfigurations(t *testing.T) {
 }
 
 func TestGetUnknownFlags(t *testing.T) {
-	// TODO
+	assert := assert.New(t)
+	var fileFlags map[string]interface{}
+	var flagSet *pflag.FlagSet
+	assert.Equal(nil, getUnknownFlags(flagSet, fileFlags))
+
+	flagSet = pflag.NewFlagSet("flagSet", pflag.ContinueOnError)
+	flagSet.String("a", "a", "")
+	flagSet.String("b", "b", "")
+	flagSet.String("c", "c", "")
+	flagSet.Parse([]string{"--a=a", "--b=b", "--c=c"})
+	assert.Equal(nil, getUnknownFlags(flagSet, fileFlags))
+
+	fileFlags = map[string]interface{}{
+		"a": "a",
+		"b": "b",
+		"c": "c",
+	}
+	assert.Equal(nil, getUnknownFlags(flagSet, fileFlags))
+
+	fileFlags = map[string]interface{}{
+		"a": "a",
+		"b": "b",
+		"c": "c",
+		"d": "d",
+		"e": "e",
+	}
+	assert.Equal(fmt.Errorf("unknown flags: d, e"), getUnknownFlags(flagSet, fileFlags))
 }
