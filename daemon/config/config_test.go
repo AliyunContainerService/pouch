965b7ae410777fa81ab01cd70a1efd6ea8b27d8b
Erhuan <553614753@qq.com>
2018-07-24 09:03:29 +0800

    add unit-test for daemon/config/getUnknownFlags
    Signed-off-by: Erhuan <553614753@qq.com>

--- daemon/config/config_test.go
+++ daemon/config/config_test.go
@@ -3,6 +3,7 @@ package config
 import (
 	"testing"
 
+	"github.com/spf13/pflag"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -53,5 +54,44 @@ func TestGetConflictConfigurations(t *testing.T) {
 }
 
 func TestGetUnknownFlags(t *testing.T) {
-	// TODO
+
+	flagSet := new(pflag.FlagSet)
+	flagSet.String("a", "a", "a")
+	flagSet.Bool("b", false, "b")
+	flagSet.Int("c", -500, "c")
+
+	flagSetNil := new(pflag.FlagSet)
+
+	assert := assert.New(t)
+
+	fileFlagsKnown := map[string]interface{}{
+		"a": "a",
+		"b": true,
+	}
+
+	fileFlagsUnknown := map[string]interface{}{
+		"c": 100,
+		"d": "d",
+	}
+
+	fileFlagsNil := map[string]interface{}{}
+
+	error := getUnknownFlags(flagSet, fileFlagsKnown)
+	assert.Equal(error, nil)
+
+	error = getUnknownFlags(flagSet, fileFlagsUnknown)
+	assert.NotNil(error)
+
+	error = getUnknownFlags(flagSet, fileFlagsNil)
+	assert.Equal(error, nil)
+
+	error = getUnknownFlags(flagSetNil, fileFlagsUnknown)
+	assert.NotNil(error)
+
+	error = getUnknownFlags(flagSetNil, fileFlagsKnown)
+	assert.NotNil(error)
+
+	error = getUnknownFlags(flagSetNil, fileFlagsNil)
+	assert.Equal(error, nil)
+
 }
