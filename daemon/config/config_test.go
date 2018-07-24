26076bd46ce8b74ab33705d2eef7f737d08131ba
Simonla <zengmingyang96@gmail.com>
2018-07-24 10:39:46 +0800

    add unit test for daemon/config/config.go: getConflictConfigurations

--- daemon/config/config_test.go
+++ daemon/config/config_test.go
@@ -3,6 +3,7 @@ package config
 import (
 	"testing"
 
+    "github.com/spf13/pflag"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -49,9 +50,38 @@ func TestConfigValidate(t *testing.T) {
 }
 
 func TestGetConflictConfigurations(t *testing.T) {
-	// TODO
+	assert := assert.New(t)
+
+	fileflags := map[string]interface{}{
+		"name_a": "value_a",
+		"name_b": "value_b",
+		"name_s": []string{"value_s1", "value_s2"},
+	}
+
+	flags := pflag.NewFlagSet("testflags", pflag.ExitOnError)
+	flags.String("name_c", "value_c", "help_c")
+
+	// While flags is not set
+	assert.Equal(nil, getConflictConfigurations(flags, fileflags))
+
+	// While no conflict
+	flags.Parse([]string{"--name_c=value_c"})
+	assert.Equal(nil, getConflictConfigurations(flags, fileflags))
+
+	// Create conflict
+	flags.String("name_a", "value_a", "help_a")
+	flags.String("name_b", "value_b", "help_b")
+	flags.Parse([]string{"--name_a=value_a", "--name_b=value_b"})
+	assert.Equal("found conflict flags in command line and config file: from flag: value_a and from config file: value_a, from flag: value_b and from config file: value_b",
+		getConflictConfigurations(flags, fileflags).Error())
+
+	// Add slice conflict, it should be ignored
+	flags.StringSlice("name_s", []string{"value_s1", "value_s2"}, "help_s")
+	flags.Parse([]string{"--name_s=value_s1,value_s2"})
+	assert.Equal("found conflict flags in command line and config file: from flag: value_a and from config file: value_a, from flag: value_b and from config file: value_b",
+		getConflictConfigurations(flags, fileflags).Error())
 }
 
 func TestGetUnknownFlags(t *testing.T) {
-	// TODO
+
 }
