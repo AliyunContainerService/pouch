1f660fda38f2c468a491680df7f7f87b8d76dffa
ly.keluo <ly.keluo@alibaba-inc.com>
2018-07-24 10:32:24 +0800

    add useProxy unit-test

--- ctrd/image_proxy_util_test.go
+++ ctrd/image_proxy_util_test.go
@@ -1,6 +1,7 @@
 package ctrd
 
 import (
+	"net/url"
 	"testing"
 )
 
@@ -35,34 +36,33 @@ func TestHasPort(t *testing.T) {
 }
 
 func TestCanonicalAddr(t *testing.T) {
-	// TODO
+	var str = "://alibaba-inc@host.com"
+
+	methods := []string{
+		"http",
+		"https",
+		"socks5",
+		"other",
+	}
+
+	for i := 0; i < len(methods); i++ {
+
+		testUrl := methods[i] + str
+
+		parseUrl, err := url.Parse(testUrl)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		addr := canonicalAddr(parseUrl)
+
+		if hasPort(addr) == false {
+			t.Fatalf("no port!!")
+		}
+	}
 }
 
 func TestUseProxy(t *testing.T) {
-	type args struct {
-		str string
-	}
-	param := []struct {
-		name string
-		args args
-		want bool
-	}{
-		// TODO: Add test cases.
-		{name: "test1", args: args{str: string("localhost:8000")}, want: false},
-		{name: "test2", args: args{str: string("[ipv6::localhost]:8000")}, want: true},
-		{name: "test3", args: args{str: string(":8000")}, want: true},
-		{name: "test4", args: args{str: string("[ipv6::127.0.0.1]::8000")}, want: false},
-		{name: "test5", args: args{str: string("localhost")}, want: false},
-		{name: "test6", args: args{str: string("[ipv6::localhost]")}, want: false},
-		{name: "test7", args: args{str: string("[ipv6::localhost]8000")}, want: false},
-	}
-	for _, tt := range param {
-		t.Run(tt.name, func(t *testing.T) {
-			got := useProxy(tt.args.str)
-			if got != tt.want {
-				t.Errorf("useProxy() = %v, want %v", got, tt.want)
-				return
-			}
-		})
-	}
+	// TODO
 }
