1a803153cb6346ef19629dd43dc0426391770606
Ace-Tang <aceapril@126.com>
2018-07-29 16:17:39 +0800

    makefile: simplify makefile output
    remove golang build command in make progress.
    
    Signed-off-by: Ace-Tang <aceapril@126.com>
    
    add unit test

--- cri/v1alpha2/cri_utils_test.go
+++ cri/v1alpha2/cri_utils_test.go
@@ -345,7 +345,7 @@ func Test_makeSandboxPouchConfig(t *testing.T) {
 		want    *apitypes.ContainerCreateConfig
 		wantErr bool
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -405,7 +405,7 @@ func Test_toCriSandbox(t *testing.T) {
 		want    *runtime.PodSandbox
 		wantErr bool
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -621,7 +621,7 @@ func Test_makeContainerName(t *testing.T) {
 		args args
 		want string
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -642,7 +642,7 @@ func Test_modifyContainerNamespaceOptions(t *testing.T) {
 		name string
 		args args
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -663,7 +663,7 @@ func Test_applyContainerSecurityContext(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -691,7 +691,7 @@ func TestCriManager_updateCreateConfig(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -716,7 +716,7 @@ func Test_toCriContainer(t *testing.T) {
 		want    *runtime.Container
 		wantErr bool
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -743,7 +743,65 @@ func Test_imageToCriImage(t *testing.T) {
 		want    *runtime.Image
 		wantErr bool
 	}{
-	// TODO: Add test cases.
+		{
+			name: "test1_no_user_name",
+			args: args{image: &apitypes.ImageInfo{
+				Config: &apitypes.ContainerConfig{
+					User: "",
+				},
+				Size: 100,},},
+			want: &runtime.Image{
+				Id:          "",
+				RepoTags:    nil,
+				RepoDigests: nil,
+				Size_:       100,
+				Uid:         &runtime.Int64Value{Value: 0},
+				Username:    "",
+			},
+			wantErr: false,
+		},
+		{
+			name: "test2_only_user_name",
+			args: args{image: &apitypes.ImageInfo{
+				Config: &apitypes.ContainerConfig{
+					User: "user_name_test",
+				},
+				Size: 100,},},
+			want: &runtime.Image{
+				Id:          "",
+				RepoTags:    nil,
+				RepoDigests: nil,
+				Size_:       100,
+				Uid:         &runtime.Int64Value{Value: 0},
+				Username:    "user_name_test",
+			},
+			wantErr: false,
+		},
+		{
+			name: "test_uid",
+			args: args{image: &apitypes.ImageInfo{
+				Config: &apitypes.ContainerConfig{
+					User: "100000:",
+				},
+				Size: 100,},},
+			want: &runtime.Image{
+				Id:          "",
+				RepoTags:    nil,
+				RepoDigests: nil,
+				Size_:       100,
+				Uid:         &runtime.Int64Value{Value: 100000},
+				Username:    "",
+			}, wantErr: false,
+		},
+		{
+			name: "test_user_id_and_name",
+			args: args{image: &apitypes.ImageInfo{
+				Config: &apitypes.ContainerConfig{User: "123:22"}}},
+			want: &runtime.Image{
+				Uid:      &runtime.Int64Value{123},
+				Username: ""},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -774,7 +832,7 @@ func TestCriManager_ensureSandboxImageExists(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -799,7 +857,7 @@ func Test_getUserFromImageUser(t *testing.T) {
 		want  *int64
 		want1 string
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -823,7 +881,7 @@ func Test_parseUserFromImageUser(t *testing.T) {
 		args args
 		want string
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
