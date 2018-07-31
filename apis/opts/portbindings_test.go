0c4f5e8d1916b5130dca6da6517c85095bcbbc81
wukai.yj <wukai.yj@wukaiyjdeMacBook-Air.local>
2018-07-31 11:17:48 +0800

    unit_test add

--- apis/opts/portbindings_test.go
+++ apis/opts/portbindings_test.go
@@ -43,6 +43,14 @@ func TestVerifyPortBinding(t *testing.T) {
 		wantErr bool
 	}{
 	// TODO: Add test cases.
+		{name:"test 1",args:args{portBindings:map[string][]types.PortBinding{"21/ftp": {types.PortBinding{HostIP:"",HostPort: "21"}},}},wantErr:false},
+		{name:"test 2",args:args{portBindings:map[string][]types.PortBinding{"21/udp": {types.PortBinding{HostIP:"",HostPort: "21"}},}},wantErr:false},
+		{name:"test 3",args:args{portBindings:map[string][]types.PortBinding{"21/ftp": {types.PortBinding{HostIP:"",HostPort: "35"}},}},wantErr:false},
+		{name:"test 4",args:args{portBindings:map[string][]types.PortBinding{"21/ftp": {types.PortBinding{HostIP:"0.0.0.0",HostPort: ""}},}},wantErr:false},
+		{name:"test 5",args:args{portBindings:map[string][]types.PortBinding{"65537/tcp": {types.PortBinding{HostIP:"",HostPort: ""}},}},wantErr:true},
+		{name:"test 6",args:args{portBindings:map[string][]types.PortBinding{"0/tcp": {types.PortBinding{HostIP:"",HostPort: ""}},}},wantErr:false},
+		{name:"test 7",args:args{portBindings:map[string][]types.PortBinding{"0/tcp": {types.PortBinding{HostIP:"0.0.0.0",HostPort: "0"}},}},wantErr:false},
+		{name:"test 8",args:args{portBindings:map[string][]types.PortBinding{"80/http": {types.PortBinding{HostIP:"",HostPort: ""}},}},wantErr:false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
