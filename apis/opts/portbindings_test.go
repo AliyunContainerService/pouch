adad393823ba2b484323dd629ea25996adeae421
gaopeiyang <gaopeiyang@126.com>
2018-07-31 10:57:28 +0800

    #1905

--- apis/opts/portbindings_test.go
+++ apis/opts/portbindings_test.go
@@ -17,13 +17,13 @@ func TestParsePortBinding(t *testing.T) {
 		want    types.PortMap
 		wantErr bool
 	}{
-		{name: "test1", args: args{ports: []string{"100:200"}}, want: types.PortMap{"200/tcp": []types.PortBinding{types.PortBinding{HostIP: "", HostPort: "100"}}}, wantErr: false},
-		{name: "test2", args: args{ports: []string{"127.0.0.1:100:200/UDP"}}, want: types.PortMap{"200/udp": []types.PortBinding{types.PortBinding{HostIP: "127.0.0.1", HostPort: "100"}}}, wantErr: false},
-		{name: "test3", args: args{ports: []string{"127.0.0.1:100-101:200/UDP"}}, want: types.PortMap{"200/udp": []types.PortBinding{types.PortBinding{HostIP: "127.0.0.1", HostPort: "100"}}}, wantErr: true},
-		{name: "test4", args: args{ports: []string{"127.0.0.1:100-101:200-201/UDP"}}, want: types.PortMap{"200/udp": []types.PortBinding{types.PortBinding{HostIP: "127.0.0.1", HostPort: "100"}}, "201/udp": []types.PortBinding{types.PortBinding{HostIP: "127.0.0.1", HostPort: "101"}}}, wantErr: false},
-		{name: "test5", args: args{ports: []string{"127.0.0.1:100:65536/UDP"}}, want: types.PortMap{"65536/udp": []types.PortBinding{types.PortBinding{HostIP: "127.0.0.1", HostPort: "100"}}}, wantErr: true},
-		{name: "test6", args: args{ports: []string{"127.0.0.1:100:65534/scc"}}, want: types.PortMap{"65534/udp": []types.PortBinding{types.PortBinding{HostIP: "127.0.0.1", HostPort: "100"}}}, wantErr: true},
-		{name: "test7", args: args{ports: []string{"[FF01::1101]:100:200/sctp"}}, want: types.PortMap{"200/sctp": []types.PortBinding{types.PortBinding{HostIP: "[FF01::1101]", HostPort: "100"}}}, wantErr: false},
+		{name: "test1", args: args{ports: []string{"100:200"}}, want: types.PortMap{"200/tcp": []types.PortBinding{{HostIP: "", HostPort: "100"}}}, wantErr: false},
+		{name: "test2", args: args{ports: []string{"127.0.0.1:100:200/UDP"}}, want: types.PortMap{"200/udp": []types.PortBinding{{HostIP: "127.0.0.1", HostPort: "100"}}}, wantErr: false},
+		{name: "test3", args: args{ports: []string{"127.0.0.1:100-101:200/UDP"}}, want: types.PortMap{"200/udp": []types.PortBinding{{HostIP: "127.0.0.1", HostPort: "100"}}}, wantErr: true},
+		{name: "test4", args: args{ports: []string{"127.0.0.1:100-101:200-201/UDP"}}, want: types.PortMap{"200/udp": []types.PortBinding{{HostIP: "127.0.0.1", HostPort: "100"}}, "201/udp": []types.PortBinding{{HostIP: "127.0.0.1", HostPort: "101"}}}, wantErr: false},
+		{name: "test5", args: args{ports: []string{"127.0.0.1:100:65536/UDP"}}, want: types.PortMap{"65536/udp": []types.PortBinding{{HostIP: "127.0.0.1", HostPort: "100"}}}, wantErr: true},
+		{name: "test6", args: args{ports: []string{"127.0.0.1:100:65534/scc"}}, want: types.PortMap{"65534/udp": []types.PortBinding{{HostIP: "127.0.0.1", HostPort: "100"}}}, wantErr: true},
+		{name: "test7", args: args{ports: []string{"[FF01::1101]:100:200/sctp"}}, want: types.PortMap{"200/sctp": []types.PortBinding{{HostIP: "[FF01::1101]", HostPort: "100"}}}, wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -48,11 +48,11 @@ func TestVerifyPortBinding(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		{name: "test1", args: args{portBindings: types.PortMap{"100": []types.PortBinding{types.PortBinding{HostIP: "", HostPort: "100"}}}}, wantErr: false},
-		{name: "test2", args: args{portBindings: types.PortMap{"-1": []types.PortBinding{types.PortBinding{HostIP: "", HostPort: "100"}}}}, wantErr: true},
-		{name: "test3", args: args{portBindings: types.PortMap{"200s": []types.PortBinding{types.PortBinding{HostIP: "", HostPort: "100"}}}}, wantErr: true},
-		{name: "test4", args: args{portBindings: types.PortMap{"200": []types.PortBinding{types.PortBinding{HostIP: "", HostPort: "100-200"}}}}, wantErr: false},
-		{name: "test5", args: args{portBindings: types.PortMap{"200": []types.PortBinding{types.PortBinding{HostIP: "", HostPort: "300-200"}}}}, wantErr: true},
+		{name: "test1", args: args{portBindings: types.PortMap{"100": []types.PortBinding{{HostIP: "", HostPort: "100"}}}}, wantErr: false},
+		{name: "test2", args: args{portBindings: types.PortMap{"-2": []types.PortBinding{{HostIP: "", HostPort: "100"}}}}, wantErr: true},
+		{name: "test3", args: args{portBindings: types.PortMap{"200s": []types.PortBinding{{HostIP: "", HostPort: "100"}}}}, wantErr: true},
+		{name: "test4", args: args{portBindings: types.PortMap{"200": []types.PortBinding{{HostIP: "", HostPort: "100-200"}}}}, wantErr: false},
+		{name: "test5", args: args{portBindings: types.PortMap{"200": []types.PortBinding{{HostIP: "", HostPort: "300-200"}}}}, wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
