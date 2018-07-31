407f90c7a63fa1f74710dffa3eb93cc6542b9843
zthbruce <zthbruce@bupt.edu.cn>
2018-07-31 08:26:24 +0530

    Mod[unit-test for apis/opts/portbindings.go]

--- apis/opts/portbindings_test.go
+++ apis/opts/portbindings_test.go
@@ -17,7 +17,6 @@ func TestParsePortBinding(t *testing.T) {
 		want    types.PortMap
 		wantErr bool
 	}{
-		// TODO: Add test cases.
 		{
 			name: "normal test",
 			args: args{ports: []string{"192.168.0.1:8080:1010"}},
@@ -127,7 +126,6 @@ func TestVerifyPortBinding(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
 		{
 			name: "test normal port",
 			args: args{
