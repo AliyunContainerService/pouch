a31b80de2180e8e70365c08f332aa4d3b4973232
zhuxiaoyuan <zxy176583@alibaba-inc.com>
2018-07-31 10:57:41 +0800

    add unit test for  apis/opts/portbindings.go

--- apis/opts/portbindings_test.go
+++ apis/opts/portbindings_test.go
@@ -44,6 +44,19 @@ func TestParsePortBinding(t *testing.T) {
 		},
 
 		{
+			name: "normal test",
+			args: args{ports: []string{"112.126.1.1:1201:1301"}},
+			want: types.PortMap{
+				"1301/tcp": []types.PortBinding{
+					types.PortBinding{
+						HostIP:"112.126.1.1",
+						HostPort:"1201",
+					},
+				},
+			},
+			wantErr:false,
+		},
+		{
 			name:"fail test",
 			args:args{ports:[]string{"112.126.1.1:1201:/tcp"}},
 			want:nil,
@@ -97,6 +110,24 @@ func TestVerifyPortBinding(t *testing.T) {
 						HostPort:"1203",
 					},
 				},
+			}},
+			wantErr:false,
+		},
+		{
+			name:"normal test",
+			args:args{types.PortMap{
+				 "1301": []types.PortBinding{
+					 types.PortBinding{
+						 HostIP:   "112.126.1.1",
+						 HostPort: "1205-1207",
+					 },
+				 },
+				 "1302": []types.PortBinding{
+				 	  types.PortBinding{
+				 	  	 HostIP:"112.126.1.1",
+				 	  	 HostPort:"1202",
+					  },
+				 } ,
 			}},
 			wantErr:false,
 		},
