c48e8dcf62df103d38c090228f2a35e1ff16b52e
StephenRi <zhaoshbnbjl@gmail.com>
2018-07-31 09:56:29 +0800

    up portbindings_test.go

--- apis/opts/portbindings_test.go
+++ apis/opts/portbindings_test.go
@@ -3,7 +3,6 @@ package opts
 import (
 	"reflect"
 	"testing"
-	"fmt"
 	"github.com/alibaba/pouch/apis/types"
 )
 
@@ -35,7 +34,6 @@ func TestParsePortBinding(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := ParsePortBinding(tt.args.ports)
-			fmt.Println(got)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ParsePortBinding() error = %v, wantErr %v", err, tt.wantErr)
 				return
