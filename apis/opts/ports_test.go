9707af997795d23d4ffc460819f5beceb8b511b3
lapis-hong <dhq1993@qq.com>
2018-07-31 10:45:20 +0800

    add ports_test

--- apis/opts/ports_test.go
+++ apis/opts/ports_test.go
@@ -16,40 +16,40 @@ func TestParseExposedPorts(t *testing.T) {
 		want    map[string]interface{}
 		wantErr bool
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 		{
-				name:	"wrongcase",
-				args:	args{portList: []string{"2222"}, expose: []string{"4444"}},
-				want:	map[string]interface{}{"2222/tcp":""},
-				wantErr:true,
+			name:    "wrongcase",
+			args:    args{portList: []string{"2222"}, expose: []string{"4444"}},
+			want:    map[string]interface{}{"2222/tcp": ""},
+			wantErr: true,
 		},
 
 		{
-				name:	"wrongcase1",
-				args:	args{portList: []string{"111a"}, expose: []string{"4444"}},
-				want:	nil,
-				wantErr:true,
+			name:    "wrongcase1",
+			args:    args{portList: []string{"111a"}, expose: []string{"4444"}},
+			want:    nil,
+			wantErr: true,
 		},
 
 		{
-				name:	"wrongcase2",
-				args:	args{portList: []string{"1111","2222","3333"}, expose: []string{"4:aa"}},
-				want:	nil,
-				wantErr:true,
+			name:    "wrongcase2",
+			args:    args{portList: []string{"1111", "2222", "3333"}, expose: []string{"4:aa"}},
+			want:    nil,
+			wantErr: true,
 		},
 
 		{
-				name:	"wrongcase3",
-				args:	args{portList: []string{"1111"}, expose: []string{"4444-4440"}},
-				want: nil,
-				wantErr:true,
+			name:    "wrongcase3",
+			args:    args{portList: []string{"1111"}, expose: []string{"4444-4440"}},
+			want:    nil,
+			wantErr: true,
 		},
 
 		{
-				name:	"case3",
-				args:	args{portList: []string{"120.202.45.45:2222:3333/tcp"}, expose: []string{"44aa"}},
-				want: nil,
-				wantErr:true,
+			name:    "case3",
+			args:    args{portList: []string{"120.202.45.45:2222:3333/tcp"}, expose: []string{"44aa"}},
+			want:    nil,
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
