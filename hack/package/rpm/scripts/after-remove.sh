c252744fe413c2c852a1fcb387248a5b0f23a578
Ace-Tang <aceapril@126.com>
2018-09-18 22:30:33 +0800

    package: add bash completion file into rpm
    install bash completion when install pouch rpm.
    
    Signed-off-by: Ace-Tang <aceapril@126.com>

--- hack/package/rpm/scripts/after-remove.sh
+++ hack/package/rpm/scripts/after-remove.sh
@@ -3,3 +3,8 @@ systemctl daemon-reload > /dev/null 2>&1
 if [ "$1" -ge 1 ] ; then
 	systemctl try-restart pouch > /dev/null 2>&1
 fi
+
+COMPLETION_FILE="contrib/completion/bash/pouch"
+if [ -f "$COMPLETION_FILE" ];then
+	rm -f $COMPLETION_FILE
+fi
