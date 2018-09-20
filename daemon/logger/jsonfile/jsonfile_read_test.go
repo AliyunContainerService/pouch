a5ed127306a5b72b1fe116d49446eef97dcf4c93
Wangrui <baijia.wr@antfin.com>
2018-09-20 17:22:08 +0800

    feature: add support for maximum log size and max number of log files
    Add max-size and max-file flags followed by --log-opt in pouchd.
    Example:
      pouchd --log-opt max-size=100m --log-opt max-file=5
    This will rotate container log when its size exceeds 100Mb, and
    it is saved in 5 log files at most.
    Note that max-size and max-file flags are only supported in jsonfile
    driver.
    
    Signed-off-by: Wangrui <baijia.wr@antfin.com>

--- daemon/logger/jsonfile/jsonfile_read_test.go
+++ daemon/logger/jsonfile/jsonfile_read_test.go
@@ -17,7 +17,7 @@ func TestReadLogMessagesWithRemoveFileInFollowMode(t *testing.T) {
 	defer f.Close()
 	defer os.RemoveAll(f.Name())
 
-	jf, err := NewJSONLogFile(f.Name(), 0640, nil)
+	jf, err := NewJSONLogFile(f.Name(), 0640, nil, nil)
 	if err != nil {
 		t.Fatalf("unexpected error during create JSONLogFile: %v", err)
 	}
@@ -52,7 +52,7 @@ func TestReadLogMessagesForEmptyFileWithoutFollow(t *testing.T) {
 	defer f.Close()
 	defer os.RemoveAll(f.Name())
 
-	jf, err := NewJSONLogFile(f.Name(), 0644, nil)
+	jf, err := NewJSONLogFile(f.Name(), 0644, nil, nil)
 	if err != nil {
 		t.Fatalf("unexpected error during create JSONLogFile: %v", err)
 	}
