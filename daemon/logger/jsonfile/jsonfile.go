dee059d3ceb9ba1165d21645a5459f6fb76893ed
zhangyue <zy675793960@yeah.net>
2018-10-12 15:09:51 +0800

    bugfix: add check for json-file log opt
    Signed-off-by: zhangyue <zy675793960@yeah.net>

--- daemon/logger/jsonfile/jsonfile.go
+++ daemon/logger/jsonfile/jsonfile.go
@@ -1,6 +1,7 @@
 package jsonfile
 
 import (
+	"fmt"
 	"os"
 	"sync"
 
@@ -56,5 +57,24 @@ func (lf *JSONLogFile) Close() error {
 		return err
 	}
 	lf.closed = true
+	return nil
+}
+
+var validLogOpt = []string{"max-file", "max-size", "compress", "labels", "env", "env-regex", "tag"}
+
+// ValidateLogOpt validate log options for json-file log driver
+func ValidateLogOpt(cfg map[string]string) error {
+	for key := range cfg {
+		isValid := false
+		for _, opt := range validLogOpt {
+			if key == opt {
+				isValid = true
+				break
+			}
+		}
+		if !isValid {
+			return fmt.Errorf("unknown log opt '%s' for json-file log driver", key)
+		}
+	}
 	return nil
 }
