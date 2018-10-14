9d3ce097fb4bc564ecad55011a9242c90b87ce5e
Tesla_Tron <Tronegacy@outlook.com>
2018-10-14 21:17:27 +0800

    golint:normalize some codes
    amend seven places' codes all is warning  with redundant if at the end of servel if.

--- daemon/mgr/spec.go
+++ daemon/mgr/spec.go
@@ -68,9 +68,5 @@ func createSpec(ctx context.Context, c *Container, specWrapper *SpecWrapper) err
 
 	// platform-specified spec setting
 	// TODO: support window and Solaris platform
-	if err := populatePlatform(ctx, c, specWrapper); err != nil {
-		return err
-	}
-
-	return nil
+	return populatePlatform(ctx, c, specWrapper)
 }
