a3b5c25ae7a8310af9238fbace3e4a1fc48e94a2
Wei Fu <fuweid89@gmail.com>
2018-10-31 19:54:08 +0800

    tools: remove package builder from pouch
    When we release pouch to yum/apt source, always needs to update builder
    script. It will change the git commit id. Sometimes, the changes doesn't
    belongs to the release version.
    
    Based on this case, using other repo to handle the package builder is
    better than current one.
    
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

--- hack/package/rpm/scripts/before-install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if [ "$1" -gt 0 ] ; then
-	rm -f /var/lib/rpm-state/pouch-is-active > /dev/null 2>&1
-	if systemctl is-active pouch > /dev/null 2>&1 ; then
-		systemctl stop pouch > /dev/null 2>&1
-		touch /var/lib/rpm-state/pouch-is-active > /dev/null 2>&1
-	fi
-fi
--- hack/package/rpm/scripts/before-remove.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ "$1" -eq 0 ] ; then
-	# Package removal
-	systemctl --no-reload disable pouch > /dev/null 2>&1
-	systemctl stop pouch > /dev/null 2>&1
-fi
