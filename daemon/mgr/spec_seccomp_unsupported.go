1d7b64e9ad6de32b7a334beca2e818b3bc2c8341
Ace-Tang <aceapril@126.com>
2018-11-01 11:11:07 +0800

    feature: support get security options in daemon
    show daemon security options, include four part,
    seccomp, apparmor, selinux and userns.
    
    Signed-off-by: Ace-Tang <aceapril@126.com>

--- daemon/mgr/spec_seccomp_unsupported.go
+++ daemon/mgr/spec_seccomp_unsupported.go
@@ -9,6 +9,11 @@ import (
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 )
 
+// IsSeccompEnable return false since pouch do not support seccomp in build
+func IsSeccompEnable() bool {
+	return false
+}
+
 func setupSeccomp(ctx context.Context, c *Container, s *specs.Spec) error {
 	if c.SeccompProfile != "" && c.SeccompProfile != "unconfined" {
 		return fmt.Errorf("Seccomp is not support by pouch, can not set seccomp profile %s", c.SeccompProfile)
