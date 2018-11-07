8467cbf9288edf64136b9464a0702fafbe4f0fba
Starnop <starnop@163.com>
2018-11-07 19:57:03 +0800

    feature: implement the feature of reopen log about cri
    Signed-off-by: Starnop <starnop@163.com>

--- cri/v1alpha2/cri_types.go
+++ cri/v1alpha2/cri_types.go
@@ -20,6 +20,9 @@ type SandboxMeta struct {
 
 	// NetNS is the sandbox's network namespace
 	NetNS string
+
+	// ContainerLogMap store the mapping of container id and CRI logPath.
+	ContainerLogMap map[string]string
 }
 
 // Key returns sandbox's id.
