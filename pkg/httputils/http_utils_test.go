c7f57ea9ef54ad5ac1daefd186e9dde219968c0a
maoyiwei <jsmyw95@qq.com>
2018-12-17 18:02:31 +0800

    test: http_utils_test: add cases for func BoolValue
    Signed-off-by: maoyiwei <jsmyw95@qq.com>

--- pkg/httputils/http_utils_test.go
+++ pkg/httputils/http_utils_test.go
@@ -2,25 +2,48 @@ package httputils
 
 import (
 	"net/http"
+	"strings"
 	"testing"
 )
 
 func TestBoolValue(t *testing.T) {
-	type args struct {
-		r *http.Request
-		k string
-	}
 	tests := []struct {
-		name string
-		args args
-		want bool
+		name    string
+		testStr string
+		want    bool
 	}{
-		// TODO: Add test cases.
+		{"test normal string", "normal string", true},
+		{"test ''", "", false},
+		{"test 0", "0", false},
+		{"test no", "no", false},
+		{"test NO", "NO", false},
+		{"test false", "false", false},
+		{"test FALSE", "FALSE", false},
+		{"test none", "none", false},
+		{"test NONE", "NONE", false},
+		// test trim
+		{"test '  '", "  ", false},
+		{"test trim 0", "  0  ", false},
+		{"test trim no", "  no  ", false},
+		{"test trim NO", "  NO ", false},
+		{"test trim false", " false ", false},
+		{"test trim FALSE", "  FALSE ", false},
+		{"test trim none", "  none ", false},
+		{"test trim NONE", "  NONE  ", false},
 	}
+	boolName := "name"
 	for _, tt := range tests {
+		request, _ := http.NewRequest(
+			"POST",
+			"http://test",
+			strings.NewReader(boolName+"="+tt.testStr),
+		)
+
+		request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
 		t.Run(tt.name, func(t *testing.T) {
-			if got := BoolValue(tt.args.r, tt.args.k); got != tt.want {
-				t.Errorf("BoolValue() = %v, want %v", got, tt.want)
+			if got := BoolValue(request, boolName); got != tt.want {
+				t.Errorf("%s, BoolValue() = %v, want %v", tt.name, got, tt.want)
 			}
 		})
 	}
