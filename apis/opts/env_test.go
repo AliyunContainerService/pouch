7783bb1ed082e76717f89d0b73a00d1a331e0041
jahentao <tzhltjh@163.com>
2018-12-19 17:22:10 +0800

    test: add unit test case for func ParseEnv
    Signed-off-by: jahentao <jahentao@qq.com>

--- apis/opts/env_test.go
+++ apis/opts/env_test.go
@@ -15,9 +15,13 @@ func TestParseEnv(t *testing.T) {
 		want    map[string]string
 		wantErr bool
 	}{
-		// TODO: Add test cases.
-		{name: "test1", args: args{env: []string{"foo=bar"}}, want: map[string]string{"foo": "bar"}, wantErr: false},
-		{name: "test2", args: args{env: []string{"ErrorInfo"}}, want: nil, wantErr: true},
+		{"test single env", args{env: []string{"foo=bar"}}, map[string]string{"foo": "bar"}, false},
+		{"test error env format", args{env: []string{"ErrorInfo"}}, nil, true},
+		{"test multiple envs", args{env: []string{"foo=bar", "A=a"}}, map[string]string{"foo": "bar", "A": "a"}, false},
+		{"test multiple '=' envs", args{env: []string{"A=1=2"}}, map[string]string{"A": "1=2"}, false},
+		{"test nil env", args{env: nil}, map[string]string{}, false}, // empty map
+		{"test empty env", args{env: []string{""}}, nil, true},
+		{"test empty blank", args{env: []string{"  "}}, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
