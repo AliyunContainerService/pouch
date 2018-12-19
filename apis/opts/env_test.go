d5292d07813a7d7be037fcc19e4504854e68f0f7
jahentao <tzhltjh@163.com>
2018-12-19 11:03:07 +0800

    test: add unit test case for func ParseEnv and ValidateEnv
    Signed-off-by: jahentao <jahentao@qq.com>

--- apis/opts/env_test.go
+++ apis/opts/env_test.go
@@ -15,9 +15,13 @@ func TestParseEnv(t *testing.T) {
 		want    map[string]string
 		wantErr bool
 	}{
-		// TODO: Add test cases.
-		{name: "test1", args: args{env: []string{"foo=bar"}}, want: map[string]string{"foo": "bar"}, wantErr: false},
-		{name: "test2", args: args{env: []string{"ErrorInfo"}}, want: nil, wantErr: true},
+		{"test single env", args{env: []string{"foo=bar"}}, map[string]string{"foo": "bar"}, false},
+		{"test error env format", args{env: []string{"ErrorInfo"}}, nil, true},
+		{"test multiple envs", args{env: []string{"foo=bar", "A=a"}}, map[string]string{"foo": "bar", "A": "a"}, false},
+		{"test multiple '=' envs", args{env: []string{"A=1=2"}}, map[string]string{"A": "1=2"}, false},
+		{"test nil env", args{env: nil}, map[string]string{}, false}, // empty map
+		{"test empty env", args{env: []string{""}}, nil, true},
+		{"test empty blank", args{env: []string{"  "}}, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -28,6 +32,36 @@ func TestParseEnv(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ParseEnv() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidateEnv(t *testing.T) {
+	type args struct {
+		env map[string]string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		valid   bool
+		wantErr bool
+	}{
+		{"test env nil", args{env: map[string]string{}}, false, true},
+		{"test env empty map", args{env: map[string]string{}}, false, true},
+		{"test single env map", args{env: map[string]string{"foo": "bar"}}, true, false},
+		{"test multiple env map", args{env: map[string]string{"foo": "bar", "A": "1=2"}}, true, false},
+		// TODO more
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			valid, err := ValidateEnv(tt.args.env)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateEnv() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(valid, tt.valid) {
+				t.Errorf("ValidateEnv() = %v, want %v", valid, tt.valid)
 			}
 		})
 	}
