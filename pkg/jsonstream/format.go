11c7dce00bd0aac44150f29ce9bd6fd47730c16c
zhangyue <zy675793960@yeah.net>
2019-01-02 09:31:48 +0800

    fix typos in comment
    Signed-off-by: zhangyue <zy675793960@yeah.net>

--- pkg/jsonstream/format.go
+++ pkg/jsonstream/format.go
@@ -6,10 +6,10 @@ import (
 
 // Formater define the data's format via stream transfer.
 type Formater interface {
-	// BegineWrite write some datas at the stream beginning.
+	// BeginWrite write some data at the stream beginning.
 	BeginWrite() ([]byte, error)
 
-	// EndWrite write some datas at the stream ending.
+	// EndWrite write some data at the stream ending.
 	EndWrite() ([]byte, error)
 
 	// Write write a object be serialized.
