fc95f8f58dbb8a949a8a71e719417d13bc7b77fa
Wei Fu <fuweid89@gmail.com>
2019-02-21 16:14:12 +0800

    pkg/cri: remove useless code
    For the user, we aleady uses the functions provided by runc vendor.
    Therefore we remove it.
    
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -27,24 +26,8 @@ var (
 
 type filterFunc func(line, str string, idInt int, idErr error) (uint32, bool)
 
-// uidParser defines lines in /etc/passwd, eg: root:x:0:0:root:/root:/bin/bash.
-type uidParser struct {
-	user        string
-	placeholder string
-	uid         int
-	gid         int
-}
-
-// gidParser defines lines in /etc/group, eg: root:x:0:.
-type gidParser struct {
-	group       string
-	placeholder string
-	gid         int
-}
-
 // Get accepts user and group slice, return valid uid, gid and additional gids.
-// Through Get is a interface returns all user informations runtime-spec need,
-// GetUser, GetIntegerID, GetAdditionalGids still can be used independently.
+// Through Get is a interface returns all user informations runtime-spec need.
 func Get(passwdPath, groupPath, username string, groups []string) (uint32, uint32, []uint32, error) {
 	logrus.Debugf("get users, passwd path: (%s), group path: (%s), username: (%s), groups: (%v)",
 		passwdPath, groupPath, username, groups)
@@ -84,93 +67,6 @@ func Get(passwdPath, groupPath, username string, groups []string) (uint32, uint3
 	}
 
 	return uid, gid, additionalGids, nil
-}
-
-// GetUser accepts user string like <uid|username>:<gid|groupname>, and transfers them to format valid uid:gid.
-// user format example:
-// user
-// uid
-// uid:gid
-// user:group
-// uid:group
-// user:gid
-func GetUser(passwdPath, groupPath, user string) (uint32, uint32, error) {
-	if user == "" {
-		// if user is null, return 0 value as root user
-		return 0, 0, nil
-	}
-
-	var (
-		uidStr, gidStr string
-		uid, gid       uint32
-		err            error
-	)
-
-	ParseString(user, &uidStr, &gidStr)
-
-	// get uid from /etc/passwd
-	uid, err = ParseID(filepath.Join(passwdPath, PasswdFile), uidStr, func(line, str string, idInt int, idErr error) (uint32, bool) {
-		var up uidParser
-		ParseString(line, &up.user, &up.placeholder, &up.uid)
-		if (idErr == nil && idInt == up.uid) || str == up.user {
-			return uint32(up.uid), true
-		}
-		return 0, false
-	})
-	if err != nil {
-		// if uidStr is a integer, treat it as valid uid
-		integer, e := strconv.Atoi(uidStr)
-		if e != nil {
-			return 0, 0, err
-		}
-		uid = uint32(integer)
-	}
-
-	// if gidStr is null, then get gid from /etc/passwd
-	if len(gidStr) == 0 {
-		gid, err = ParseID(filepath.Join(passwdPath, PasswdFile), uidStr, func(line, str string, idInt int, idErr error) (uint32, bool) {
-			var up uidParser
-			ParseString(line, &up.user, &up.placeholder, &up.uid, &up.gid)
-			if (idErr == nil && idInt == up.uid) || str == up.user {
-				return uint32(up.gid), true
-			}
-			return 0, false
-		})
-	} else {
-		gid, err = ParseID(filepath.Join(groupPath, GroupFile), gidStr, func(line, str string, idInt int, idErr error) (uint32, bool) {
-			var gp gidParser
-			ParseString(line, &gp.group, &gp.placeholder, &gp.gid)
-			if (idErr == nil && idInt == gp.gid) || str == gp.group {
-				return uint32(gp.gid), true
-			}
-			return 0, false
-		})
-	}
-	if err != nil {
-		// if gidStr is a integer, treat it as valid gid
-		integer, e := strconv.Atoi(gidStr)
-		if e != nil {
-			return 0, 0, err
-		}
-		gid = uint32(integer)
-	}
-
-	return uid, gid, nil
-}
-
-// GetIntegerID only parser user format uid:gid, cause container rootfs is not created
-// by contianerd now, can not change user to id, only support user id >= 1000
-// TODO(huamin.thm): removed later
-func GetIntegerID(user string) (uint32, uint32) {
-	if user == "" {
-		// return default user root
-		return 0, 0
-	}
-
-	// if uid gid can not be parsed successfully, return default user root
-	var uid, gid int
-	ParseString(user, &uid, &gid)
-	return uint32(uid), uint32(gid)
 }
 
 // GetAdditionalGids parse supplementary gids from slice groups.
