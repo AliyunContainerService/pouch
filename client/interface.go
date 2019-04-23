915858aa5cf5a0b2b6b4a637373b07b16fd8840f
Rudy Zhang <rudyflyzhang@gmail.com>
2019-04-23 17:01:51 +0800

    feature: add resize exec for pouch cli
    add resize exec window for pouch cli `pouch exec`
    
    Signed-off-by: Rudy Zhang <rudyflyzhang@gmail.com>

--- client/interface.go
+++ client/interface.go
@@ -28,8 +28,9 @@ type ContainerAPIClient interface {
 	ContainerList(ctx context.Context, option types.ContainerListOptions) ([]*types.Container, error)
 	ContainerAttach(ctx context.Context, name string, stdin bool) (net.Conn, *bufio.Reader, error)
 	ContainerCreateExec(ctx context.Context, name string, config *types.ExecCreateConfig) (*types.ExecCreateResp, error)
-	ContainerStartExec(ctx context.Context, execid string, config *types.ExecStartConfig) (net.Conn, *bufio.Reader, error)
-	ContainerExecInspect(ctx context.Context, execid string) (*types.ContainerExecInspect, error)
+	ContainerStartExec(ctx context.Context, execID string, config *types.ExecStartConfig) (net.Conn, *bufio.Reader, error)
+	ContainerExecInspect(ctx context.Context, execID string) (*types.ContainerExecInspect, error)
+	ContainerExecResize(ctx context.Context, execID string, options types.ResizeOptions) error
 	ContainerGet(ctx context.Context, name string) (*types.ContainerJSON, error)
 	ContainerRename(ctx context.Context, id string, name string) error
 	ContainerRestart(ctx context.Context, name string, timeout string) error
