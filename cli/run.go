e90f1b1b5637baa67945382e13bfefec28b54907
lang chi <21860405@zju.edu.cn>
2019-05-24 19:07:44 +0800

    env-file support for pouch run
    Signed-off-by: lang chi <21860405@zju.edu.cn>

--- cli/run.go
+++ cli/run.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"io"
@@ -182,6 +183,57 @@ func (rc *RunCommand) runRun(args []string) error {
 	return nil
 }
 
+func readKVStrings(files []string, override []string) ([]string, error){
+	envVariables := []string{}
+	for _,ef:=range files{
+		parsedVars, err := ParseEnvFile(ef)
+		if err != nil {
+			return nil, err
+		}
+		envVariables = append(envVariables, parsedVars...)
+	}
+	envVariables = append(envVariables, override...)
+	return envVariables, nil
+}
+
+func ParseEnvFile(filename string)([]string, error){
+	fh, err:=os.Open(filename)
+	if err!=nil{
+		return []string{}, err
+	}
+	defer fh.Close()
+
+	lines := []string{}
+	scanner:=bufio.NewScanner(fh)
+	for scanner.Scan(){
+		line:=strings.TrimLeft(scanner.Text(), whiteSpaces)
+		if len(line)>0&&!strings.HasPrefix(line, "#"){
+			data := strings.SplitN(line, "=", 2)
+			variable:=strings.TrimLeft(data[0], whiteSpaces)
+			if strings.ContainsAny(variable, whiteSpaces){
+				return []string{}, ErrBadEnvVariable{fmt.Sprintf("variable '%s' has white spaces", variable)}
+			}
+
+			if  len(data)>1{
+				lines=append(lines, fmt.Sprintf("%s=%s", variable, data[1]))
+			}else{
+				lines=append(lines, fmt.Sprintf("%s=%s", strings.TrimSpace(line), os.Getenv(line)))
+			}
+		}
+	}
+	return lines, scanner.Err()
+}
+
+var whiteSpaces=" \t"
+
+type ErrBadEnvVariable struct{
+	msg string
+}
+
+func (e ErrBadEnvVariable) Error() string{
+	return fmt.Sprintf("poorly formatted environment: %s", e.msg)
+}
+
 
 // runExample shows examples in run command, and is used in auto-generated cli docs.
 func runExample() string {
@@ -198,4 +250,5 @@ test   90719b   stopped   registry.hub.docker.com/library/busybox:latest   runc 
 $ pouch run --device /dev/zero:/dev/testDev:rwm --name test registry.hub.docker.com/library/busybox:latest ls -l /dev/testDev
 crw-rw-rw-    1 root     root        1,   3 Jan  8 09:40 /dev/testnull
 	`
+
 }
