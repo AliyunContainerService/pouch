9112076031bcdf3a2fc34363c0bddbe9b5702fb4
jerryzhuangqh <zhuangqhc@gmail.com>
2019-07-03 19:49:52 +0800

    using config file to run daemon test
    *background*: some env may depend on some
    specific config options.
    
    Signed-off-by: zhuangqh <zhuangqhc@gmail.com>

--- test/cli_logs_test.go
+++ test/cli_logs_test.go
@@ -251,7 +251,9 @@ func (suite *PouchLogsSuite) TestSetLogPathInDaemon(c *check.C) {
 	logRootDir := "/tmp/TestSetLogsPathInDaemon"
 	defer os.RemoveAll(logRootDir)
 
-	dcfg, err := StartDefaultDaemon("--log-opt", fmt.Sprintf("root-dir=%s", logRootDir))
+	dcfg, err := StartDefaultDaemon(map[string]interface{}{
+		"--log-opt": fmt.Sprintf("root-dir=%s", logRootDir),
+	})
 	c.Assert(err, check.IsNil)
 
 	defer dcfg.KillDaemon()
