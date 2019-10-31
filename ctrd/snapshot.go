e8ee069bf63657c9022649dda3cb0fa605dea9c0
Wei Fu <fuweid89@gmail.com>
2019-10-31 12:58:18 +0800

    ctrd: try to unpack image when create snapshot
    PouchContainer always unpacks for pulled images. But if there is any
    crash or terminated, the unpack action will be stopped. And image
    metadata is stored in containerd. It will cause that the following
    create container request fails. Try to unpack it if fail.
    
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

--- ctrd/snapshot.go
+++ ctrd/snapshot.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/alibaba/pouch/pkg/log"
+	"github.com/containerd/containerd/errdefs"
 	"github.com/containerd/containerd/leases"
 	"github.com/containerd/containerd/mount"
-	"github.com/containerd/containerd/platforms"
 	"github.com/containerd/containerd/snapshots"
 	"github.com/opencontainers/image-spec/identity"
 )
@@ -38,20 +39,59 @@ func (c *Client) CreateSnapshot(ctx context.Context, id, ref string) error {
 	if err != nil {
 		return fmt.Errorf("failed to get a containerd grpc client: %v", err)
 	}
+
+	original_ctx := ctx
 	ctx = leases.WithLease(ctx, wrapperCli.lease.ID)
 
-	image, err := wrapperCli.client.ImageService().Get(ctx, ref)
+	var (
+		snName = CurrentSnapshotterName(ctx)
+		snSrv  = wrapperCli.client.SnapshotService(snName)
+	)
+
+	image, err := wrapperCli.client.GetImage(ctx, ref)
 	if err != nil {
 		return err
 	}
 
-	diffIDs, err := image.RootFS(ctx, wrapperCli.client.ContentStore(), platforms.Default())
+	diffIDs, err := image.RootFS(ctx)
 	if err != nil {
 		return err
 	}
-
 	parent := identity.ChainID(diffIDs).String()
-	_, err = wrapperCli.client.SnapshotService(CurrentSnapshotterName(ctx)).Prepare(ctx, id, parent)
+
+	// NOTE: PouchContainer always unpacks image during pulling. But there
+	// maybe crash or terminated by some reason. The image have been stored
+	// in containerd without unpacking. And the following creating container
+	// request will fail on preparing snapshot because there is no such
+	// parent snapshotter. Based on this case, we should skip the not
+	// found error and try to unpack it again.
+	_, err = snSrv.Prepare(ctx, id, parent)
+	if err == nil || !errdefs.IsNotFound(err) {
+		return err
+	}
+	log.With(ctx).Warnf("checking unpack status for image %s on %s snapshotter...", image.Name(), snName)
+
+	// check unpacked status
+	unpacked, werr := image.IsUnpacked(ctx, snName)
+	if werr != nil {
+		log.With(ctx).Warnf("failed to check unpack status for image %s on %s snapshotter: %v", image.Name(), snName, werr)
+		return err
+	}
+
+	// if it is not unpacked, try to unpack it.
+	if !unpacked {
+		log.With(ctx).Warnf("the image %s doesn't unpack for %s snapshotter, try to unpack it...", image.Name(), snName)
+		// NOTE: don't use pouchd lease id here because pouchd lease id
+		// will hold the snapshotter forever, which means that the
+		// snapshotter will not removed if we remove image.
+		if werr = image.Unpack(original_ctx, snName); werr != nil {
+			log.With(ctx).Warnf("failed to unpack for image %s on %s snapshotter: %v", image.Name(), snName, werr)
+			return err
+		}
+
+		// do it again.
+		_, err = snSrv.Prepare(ctx, id, parent)
+	}
 	return err
 }
 
